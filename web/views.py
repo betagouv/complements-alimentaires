95083359245ec92cce37a20f9d9d9f5262f4f88b
Alejandro MG <alejandro@amguillen.dev>
2023-11-08 14:53:21 +0100

    Auth views

--- web/views.py
+++ web/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView, FormView
 
 class VueAppDisplayView(TemplateView):
     """
     This template contains the VueJS app in /frontend
     """
     template_name = "vue-app.html"
+
+class RegisterUserView(FormView):
+    """
+    View containing the user-only form to create an account
+    """
+
+    form_class = UserCreationForm
+    template_name = "auth/register.html"
