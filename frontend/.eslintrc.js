5f480367630cec580fdb570fabae7fa75b137e3a
Alejandro MG <alejandro@amguillen.dev>
2023-11-13 21:50:42 +0100

    Dependencies updates for npm, precommit and configuration files

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+  },
+
+  rules: {
+    "vue/no-mutating-props": "off",
+    "vue/multi-word-component-names": "off",
+    "vue/no-v-text-v-html-on-component": "off",
+    "vue/no-multiple-template-root": "off",
+  },
+  plugins: ["prettier"],
+
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
