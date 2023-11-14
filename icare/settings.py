3d834bea7b5c01ff496b902b6ab60488a64222f8
Alejandro MG <alejandro@amguillen.dev>
2023-11-14 17:03:49 +0100

    Removes unnecessary APPEND_SLASH setting

--- icare/settings.py
+++ icare/settings.py
@@ -18,9 +18,6 @@ import dotenv  # noqa
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-APPEND_SLASH = False
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
