f6113083b8b9aa8a75a1c96811da1211b61c7aca
Alejandro MG <alejandro@amguillen.dev>
2023-11-18 23:50:29 +0100

    Adds validation and calls to the backend

--- frontend/src/components/NewsletterBlock.vue
+++ frontend/src/components/NewsletterBlock.vue
@@ -5,21 +5,62 @@
       <div>Recevez tous les mois une actualisation réglementaire, des nouvelles de la plateforme teleicare....</div>
     </div>
     <div class="col-span-12 md:col-span-5 my-6 md:my-0">
-      <div class="flex">
-        <DsfrInput v-model="email" :placeholder="placeholder" />
-        <DsfrButton label="Valider" @click="onClick" />
-      </div>
+      <DsfrInputGroup :error-message="errorMessage" :valid-message="validMessage">
+        <div class="flex">
+          <DsfrInput v-model="subscriptionEmail" :placeholder="placeholder" @keydown.enter="subscribe" />
+          <DsfrButton :disabled="requestInProgress" label="Valider" @click="subscribe" />
+        </div>
+      </DsfrInputGroup>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue"
+import { ref, computed } from "vue"
+import { useVuelidate } from "@vuelidate/core"
+import { required, email } from "@vuelidate/validators"
 
-const email = ref("")
 const placeholder = "Votre email"
+const validMessage = ref()
+const errorMessage = ref()
+const requestInProgress = ref(false)
+const subscriptionEmail = ref("")
+const rules = computed(() => ({
+  subscriptionEmail: { required, email },
+}))
+const v$ = useVuelidate(rules, { subscriptionEmail })
 
-function onClick() {
-  console.log("clicked")
+function subscribe() {
+  v$.value.$validate()
+
+  if (v$.value.$error) {
+    errorMessage.value = "Ce champ doit contenir un email valide"
+    validMessage.value = null
+    return
+  }
+
+  const headers = {
+    "X-CSRFToken": window.CSRF_TOKEN || "",
+    "Content-Type": "application/json",
+  }
+  requestInProgress.value = true
+
+  return fetch("/api/v1/subscribeNewsletter/", {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ email: subscriptionEmail.value }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        errorMessage.value = null
+        validMessage.value = "Votre subscription a bien été prise en compte"
+      } else {
+        errorMessage.value = "Une erreur est survenue, veuillez réessayer plus tard."
+        validMessage.value = null
+      }
+    })
+    .finally(() => {
+      requestInProgress.value = false
+    })
 }
 </script>
