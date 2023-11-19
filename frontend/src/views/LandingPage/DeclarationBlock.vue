eb30de7f9ef1f2cfa00791da1bf9a1be20d18d71
Alejandro MG <alejandro@amguillen.dev>
2023-11-19 14:43:22 +0100

    Adds block for professionals wanting to declare

---
+++ frontend/src/views/LandingPage/DeclarationBlock.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="grid grid-cols-12">
+    <div class="col-span-12 md:col-span-7 md:pr-10">
+      <h3 class="mb-1">
+        Vous êtes une professionnelle ou un proffessionel du secteur et souhaitez déclarer un produit ?
+      </h3>
+      <p class="my-4">Suivez notre checklist pour vous orienter vers la bonne démarche de déclaration</p>
+      <DsfrButton label="Préparer ma télédéclaration" />
+    </div>
+    <div class="hidden md:flex md:col-span-5 justify-center content-center">
+      <img
+        style="max-width: 350px; object-fit: contain"
+        src="/static/images/illustrations/declaration-illustration.png"
+        alt="Professionnelle effectuant sa déclaration"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
