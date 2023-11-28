1d06ae584865b511b7ea7a881b9bb3eeb262a49d
Alejandro MG <alejandro@amguillen.dev>
2023-11-28 15:47:28 +0100

    Backend endpoints and tests for logged user API call

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -1,1 +1,2 @@
 from .blogpost import BlogPostSerializer  # noqa: F401
+from .user import LoggedUserSerializer  # noqa: F401
--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,2 +1,3 @@
 from .blog import BlogPostsView, BlogPostView  # noqa: F401
 from .newsletter import SubscribeNewsletter  # noqa: F401
+from .user import LoggedUserView  # noqa: F401
