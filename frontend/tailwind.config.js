db29dd5b4c3aed379c11d07010b78b9bdac28c46
Alejandro MG <alejandro@amguillen.dev>
2023-12-06 15:17:58 +0100

    Aligns breakpoints from DSFR and Tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js,vue}"],
   theme: {
     extend: {},
+
+    // DSFR breakpoints taken from here:
+    // https://github.com/dnum-mi/vue-dsfr/blob/e363297144888021f3d4379c13ef0164ba80a90e/.storybook/preview.ts#L44
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1440px",
+    },
   },
   plugins: [],
 }
