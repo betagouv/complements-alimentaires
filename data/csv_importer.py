32f816343b3301ad79701bf7bcec631e3acfaedf
Perrine Letellier <perrine.letellier@pm.me>
2023-12-19 16:26:49 +0100

    make more lisible

--- data/csv_importer.py
+++ data/csv_importer.py
@@ -12,15 +12,9 @@ from .models.plant import Plant, PlantPart, PlantSynonym, PlantFamily
 from .models.population import Population
 from .models.substance import Substance, SubstanceSynonym
 
-
-# TODO : renseigner l'update dans l'historique de chaque objet
-# TODO : lors de l'import d'un fichier CSV correspondant à un modèle, il faudrait forcer le fait d'importer aussi ses relations
-# TODO : prévoir l'import de fichiers différents provenant de World Flora par exemple
-# TODO : il faut qu'on utilise leur primary keys
-
 logger = logging.getLogger(__name__)
 
-# Établi dans quel modèle les données d'un fichier csv doivent être importées
+# Modèles pour recevoir l'import des données des fichier csv
 CSV_TO_MODEL_MAPPING = {
     "REF_ICA_INGREDIENT_AUTRE.csv": Ingredient,
     "REF_ICA_MICRO_ORGANISME.csv": Microorganism,
@@ -31,13 +25,13 @@ CSV_TO_MODEL_MAPPING = {
     "REF_ICA_PLANTE_SYNONYME.csv": PlantSynonym,
     "REF_ICA_SUBSTANCE_ACTIVE_SYNONYME.csv": SubstanceSynonym,
     "POPULATION.CSV": Population,
-    # 'OBJECTIF.CSV': 'objectif',
+    # 'OBJECTIF.CSV': Objectif,
+    # 'FICHIERA_RECUPERER.CSV': PlantFamily,
     # Les csv avec les relations ManyToMany
     # 'REF_ICA_AUTREING_SUBSTACTIVE.csv': 'autreing_substactive',
     # 'REF_ICA_PLANTE_SUBSTANCE.csv': 'plante_substance',
     # 'REF_ICA_PARTIE_PL_A_SURVEILLER.csv': 'partie_pl_a_surveiller',
     # 'REF_ICA_PARTIE_UTILE.csv': 'partie_utile'
-    # TODO : récupérer le fichier pour PlantFamily
 }
 
 # Établi le préfix des champs du csv
@@ -112,65 +106,6 @@ def _get_model_from_csv_name(csv_filename):
     return CSV_TO_MODEL_MAPPING[csv_filename]
 
 
-def _get_model_fields(model):
-    automatically_filled = ["id", "creation_date", "modification_date"]
-    return [field for field in model._meta.fields if field.name not in automatically_filled]
-
-
-def _get_column_name(field_name, csv_fields_in_header, csv_filename, prefixed=True):
-    csv_field_names = DJANGO_FIELD_NAME_TO_CSV_FIELD_NAME_MAPPING[field_name]
-    if prefixed:
-        prefix = CSV_TO_TABLE_PREFIX_MAPPING[csv_filename]
-        csv_field_names = [f"{prefix}_{csv_field_name}" for csv_field_name in csv_field_names]
-
-    csv_field_name = list(set(csv_field_names) & set(csv_fields_in_header))[0]
-    return csv_field_name
-
-
-def _get_linked_model(column_name):
-    if not column_name.endswith("IDENT"):
-        logger.error(f"{column_name} n'est pas une colonne contenant une clé étrangère.")
-        return
-    else:
-        foreign_key_prefix = column_name.split("_")[0]
-        model = PREFIX_TO_MODEL_MAPPINT[foreign_key_prefix]
-        return model
-
-
-def _create_django_fields_to_column_names_mapping(model, csv_fieldnames, csv_filename):
-    """
-    Creation du mapping entre les nom de champs des modèles django et les nom de colonnes dans les csv
-    """
-
-    django_fields = _get_model_fields(model)
-    django_fields_to_column_names = {}
-    for field in django_fields:
-        # le nom des colonnes contenant les clés étrangères ne sont pas préfixées par le nom de la table
-        prefixed = False if isinstance(field, ForeignKey) else True
-        try:
-            column_name = _get_column_name(field.name, csv_fieldnames, csv_filename, prefixed=prefixed)
-            django_fields_to_column_names[field] = column_name
-        except IndexError as e:
-            logger.warning(f"Ce champ n'existe pas dans le csv' : {e}")
-    return django_fields_to_column_names
-
-
-def _clean_value(value, field):
-    if value == "NULL":
-        if isinstance(field, TextField) or isinstance(field, CharField):
-            return ""
-        else:
-            return None
-    if isinstance(field, FloatField):
-        try:
-            # la virgule est considérée dans son usage français des nombres décimaux
-            float_value = float(value.replace(",", "."))
-            return float_value
-        except ValueError:
-            return value
-    return value
-
-
 def _import_csv_to_model(csv_filepath, model):
     csv_filename = os.path.basename(csv_filepath)
     nb_success = 0
@@ -206,3 +141,64 @@ def _import_csv_to_model(csv_filepath, model):
             _ = model.objects.update_or_create(**object_definition)
             nb_success += 1
     return nb_success
+
+
+def _get_model_fields_to_complete(model):
+    automatically_filled = ["id", "creation_date", "modification_date"]
+    return [field for field in model._meta.fields if field.name not in automatically_filled]
+
+
+def _get_column_name(field_name, csv_fields_in_header, csv_filename, prefixed=True):
+    csv_field_names = DJANGO_FIELD_NAME_TO_CSV_FIELD_NAME_MAPPING[field_name]
+    if prefixed:
+        prefix = CSV_TO_TABLE_PREFIX_MAPPING[csv_filename]
+        csv_field_names = [f"{prefix}_{csv_field_name}" for csv_field_name in csv_field_names]
+
+    csv_field_name = list(set(csv_field_names) & set(csv_fields_in_header))[0]
+    return csv_field_name
+
+
+def _get_linked_model(column_name):
+    """
+    Récupération du modèle correspondante au nom de colonne d'une clé étrangère
+    """
+    if not column_name.endswith("IDENT"):
+        logger.error(f"{column_name} n'est pas une colonne contenant une clé étrangère.")
+        return
+    else:
+        foreign_key_prefix = column_name.split("_")[0]
+        model = PREFIX_TO_MODEL_MAPPINT[foreign_key_prefix]
+        return model
+
+
+def _create_django_fields_to_column_names_mapping(model, csv_fieldnames, csv_filename):
+    """
+    Creation du mapping entre les nom de champs des modèles django et les nom de colonnes dans les csv
+    """
+    django_fields = _get_model_fields_to_complete(model)
+    django_fields_to_column_names = {}
+    for field in django_fields:
+        # le nom des colonnes contenant les clés étrangères ne sont pas préfixées par le nom de la table
+        prefixed = False if isinstance(field, ForeignKey) else True
+        try:
+            column_name = _get_column_name(field.name, csv_fieldnames, csv_filename, prefixed=prefixed)
+            django_fields_to_column_names[field] = column_name
+        except IndexError as e:
+            logger.warning(f"Ce champ n'existe pas dans le csv' : {e}")
+    return django_fields_to_column_names
+
+
+def _clean_value(value, field):
+    if value == "NULL":
+        if isinstance(field, TextField) or isinstance(field, CharField):
+            return ""
+        else:
+            return None
+    if isinstance(field, FloatField):
+        try:
+            # la virgule est considérée dans son usage français des nombres décimaux
+            float_value = float(value.replace(",", "."))
+            return float_value
+        except ValueError:
+            return value
+    return value
