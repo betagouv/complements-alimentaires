aa0b231c17f4e03daa133f67844b6fb54ed8d008
Perrine Letellier <perrine.letellier@pm.me>
2024-01-15 18:44:04 +0100

    add environment tag to Vue frontend

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,5 +1,9 @@
 <template>
   <DsfrHeader :logo-text="logoText" service-title="Compléments alimentaires" :quickLinks="quickLinks">
+    <template v-if="environment != 'prod'" #operator>
+      <DsfrBadge v-if="environment === 'dev'" :label="environment" type="info" />
+      <DsfrBadge v-else :label="environment" type="warning" />
+    </template>
     <template #mainnav>
       <DsfrNavigation :nav-items="navItems" />
     </template>
@@ -11,6 +15,7 @@ import { computed, onMounted } from "vue"
 import { useStore } from "vuex"
 
 const logoText = ["Ministère", "de l'Agriculture", "et de la Souveraineté", "Alimentaire"]
+const environment = window.ENVIRONMENT
 const store = useStore()
 const navItems = [
   {
