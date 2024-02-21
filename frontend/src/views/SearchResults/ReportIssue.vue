20e5a74ede6c2e3fbb2d1ecb61f1811795c176f5
David Dahan <ddahan@users.noreply.github.com>
2024-02-21 16:38:30 +0100

    Add Toaster/Snackbar feature (#239)

--- frontend/src/views/SearchResults/ReportIssue.vue
+++ frontend/src/views/SearchResults/ReportIssue.vue
@@ -8,53 +8,43 @@
       </p>
     </div>
     <div class="col-span-12 md:col-span-5 my-6 md:my-0">
-      <template v-if="!isFinished">
-        <DsfrInputGroup :error-message="firstErrorMsg(v$, 'email')">
-          <DsfrInput v-model="state.email" placeholder="Votre email (optionnel)" />
-        </DsfrInputGroup>
-        <DsfrInputGroup :error-message="firstErrorMsg(v$, 'reportMessage')">
-          <DsfrInput
-            v-model="state.reportMessage"
-            placeholder="Quel(s) problème(s) constatez-vous ?"
-            :isTextarea="true"
-          />
-        </DsfrInputGroup>
-        <div class="text-right">
-          <DsfrButton :disabled="isFetching" label="Valider" @click="submit" />
-        </div>
-      </template>
-      <DsfrAlert
-        v-if="isFinished && !error"
-        type="success"
-        title="C'est envoyé !"
-        description="Votre message a bien été envoyé. Merci pour votre contribution."
-      />
-      <DsfrAlert
-        v-if="isFinished && error"
-        type="error"
-        title="Erreur"
-        description="Une erreur est survenue, veuillez réessayer plus tard."
-      />
+      <DsfrInputGroup :error-message="firstErrorMsg(v$, 'email')">
+        <DsfrInput v-model="state.email" placeholder="Votre email (optionnel)" />
+      </DsfrInputGroup>
+      <DsfrInputGroup :error-message="firstErrorMsg(v$, 'reportMessage')">
+        <DsfrInput
+          v-model="state.reportMessage"
+          placeholder="Quel(s) problème(s) constatez-vous ?"
+          :isTextarea="true"
+        />
+      </DsfrInputGroup>
+      <div class="text-right">
+        <DsfrButton :disabled="isFetching" label="Valider" @click="submit" />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { reactive } from "vue"
+import { ref } from "vue"
 import { useVuelidate } from "@vuelidate/core"
 import { required, email, helpers } from "@vuelidate/validators"
 import { useFetch } from "@vueuse/core"
 import { headers, firstErrorMsg } from "@/utils"
+import useToaster from "@/composables/use-toaster"
 
 // Props
 const props = defineProps({ elementName: String })
 
 // Form state & rules
-const state = reactive({
+const getInitialState = () => ({
+  name: "",
   email: "",
-  reportMessage: "",
   elementName: props.elementName, // not used by the form validation itself, but make the payload building easier
 })
+
+const state = ref(getInitialState())
+
 const rules = {
   email: { email: helpers.withMessage("Ce champ doit contenir un email valide s'il est spécifié", email) },
   reportMessage: { required: helpers.withMessage("Ce champ doit contenir vos constatations", required) },
@@ -72,11 +62,24 @@ const { error, execute, isFetching, isFinished } = useFetch(
 ).post(state)
 
 // Form validation
-const submit = () => {
+const submit = async () => {
   v$.value.$validate()
   if (v$.value.$error) {
     return // prevent API call if there is a front-end error
   }
-  execute()
+  await execute()
+
+  if (isFinished) {
+    useToaster().addMessage({
+      type: error.value ? "error" : "success",
+      title: error.value ? "Erreur" : "C'est envoyé !",
+      description: error.value
+        ? "Une erreur est survenue, veuillez réessayer plus tard."
+        : "Votre message a bien été envoyé. Merci pour votre contribution.",
+    })
+    // Reset both form state & Vuelidate validation state
+    state.value = getInitialState()
+    v$.value.$reset()
+  }
 }
 </script>
