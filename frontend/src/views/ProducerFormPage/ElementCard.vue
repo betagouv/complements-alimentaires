57cfccf2895b15fd446f2d67c1e77565d8c3d307
Alejandro MG <alejandro@amguillen.dev>
2024-03-11 14:36:20 +0100

    Renames ElementCard model to avoid duplicated property

--- frontend/src/views/ProducerFormPage/ElementCard.vue
+++ frontend/src/views/ProducerFormPage/ElementCard.vue
@@ -2,16 +2,16 @@
   <div class="p-4 border shadow-md">
     <div class="sm:flex">
       <div class="flex">
-        <div :class="`mr-4 self-center justify-center rounded-full icon-${element.element.objectType} h-8 w-8 flex`">
-          <v-icon class="self-center" fill="white" :name="getTypeIcon(element.element.objectType)" />
+        <div :class="`mr-4 self-center justify-center rounded-full icon-${model.element.objectType} h-8 w-8 flex`">
+          <v-icon class="self-center" fill="white" :name="getTypeIcon(model.element.objectType)" />
         </div>
         <div class="self-center">
           <div class="font-bold capitalize">
-            {{ element.element.name.toLowerCase() }}
-            <span class="uppercase text-gray-400 text-sm ml-2">{{ getType(element.element.objectType) }}</span>
+            {{ model.element.name.toLowerCase() }}
+            <span class="uppercase text-gray-400 text-sm ml-2">{{ getType(model.element.objectType) }}</span>
           </div>
-          <div v-if="element.element.synonyms?.length">
-            {{ element.element.synonyms.map((x) => x.name).join(", ") }}
+          <div v-if="model.element.synonyms?.length">
+            {{ model.element.synonyms.map((x) => x.name).join(", ") }}
           </div>
         </div>
       </div>
@@ -19,8 +19,8 @@
         <div class="grow pl-4 ml-6 sm:border-l self-center">
           <DsfrCheckbox
             class="!my-2"
-            v-model="element.element.active"
-            :label="element.element.active ? 'Actif' : 'Non actif'"
+            v-model="model.element.active"
+            :label="model.element.active ? 'Actif' : 'Non actif'"
           />
         </div>
         <DsfrButton secondary @click="$emit('remove', element)">Enlever</DsfrButton>
@@ -28,38 +28,38 @@
     </div>
     <div v-if="showFields">
       <hr class="mt-2" />
-      <div v-if="element.element.objectType === 'plant'" class="md:ml-12 block sm:flex gap-2 md:gap-4">
+      <div v-if="model.element.objectType === 'plant'" class="md:ml-12 block sm:flex gap-2 md:gap-4">
         <DsfrInputGroup class="max-w-sm" v-if="plantParts.length > 0">
           <DsfrSelect
             label="Partie utilisée"
             defaultUnselectedText=""
-            v-model="element.plantPart"
+            v-model="model.plantPart"
             :options="plantParts"
             :required="true"
           />
         </DsfrInputGroup>
         <DsfrInputGroup class="max-w-28">
-          <DsfrInput label="Qté par DJR" v-model="element.quantity" label-visible :required="true" />
+          <DsfrInput label="Qté par DJR" v-model="model.quantity" label-visible :required="true" />
         </DsfrInputGroup>
         <DsfrInputGroup class="min-w-20 max-w-24">
-          <DsfrSelect label="Unité" :options="units" v-model="element.unit" defaultUnselectedText="" :required="true" />
+          <DsfrSelect label="Unité" :options="units" v-model="model.unit" defaultUnselectedText="" :required="true" />
         </DsfrInputGroup>
         <DsfrInputGroup class="max-w-sm">
           <DsfrSelect
             label="Préparation"
             :options="preparations"
-            v-model="element.preparation"
+            v-model="model.preparation"
             defaultUnselectedText=""
             :required="true"
           />
         </DsfrInputGroup>
       </div>
-      <div v-else-if="element.element.objectType === 'microorganism'" class="ml-12 flex gap-4">
+      <div v-else-if="model.element.objectType === 'microorganism'" class="ml-12 flex gap-4">
         <DsfrInputGroup class="max-w-sm">
-          <DsfrInput label-visible label="Souche" v-model="element.strain" :required="true" />
+          <DsfrInput label-visible label="Souche" v-model="model.strain" :required="true" />
         </DsfrInputGroup>
         <DsfrInputGroup>
-          <DsfrInput label-visible v-model="element.cfu_quantity" label="Qté par DJR (en CFU)" :required="true" />
+          <DsfrInput label-visible v-model="model.cfu_quantity" label="Qté par DJR (en CFU)" :required="true" />
         </DsfrInputGroup>
       </div>
     </div>
@@ -70,14 +70,14 @@
 import { computed, defineModel } from "vue"
 import { getTypeIcon, getType } from "@/utils/mappings"
 
-const element = defineModel()
+const model = defineModel()
 
-const plantParts = computed(() => element.value.element.plantParts.map((x) => ({ text: x.name, value: x.id })))
+const plantParts = computed(() => model.value.element.plantParts.map((x) => ({ text: x.name, value: x.id })))
 const showFields = computed(
   () =>
-    element.value.element.active &&
-    element.value.element.objectType !== "substance" &&
-    element.value.element.objectType !== "ingredient"
+    model.value.element.active &&
+    model.value.element.objectType !== "substance" &&
+    model.value.element.objectType !== "ingredient"
 )
 
 // TODO: s'assurer que les unités utilisées sont les mêmes partout, et possiblement les mettre dans la base de données
