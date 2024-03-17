eafa1986decf2386fe1b40bdade2962dfd70797e
ddahan <david.dahan3@gmail.com>
2024-03-17 09:53:45 +0000

    Add 404 redirect and comments

--- frontend/src/utils/error-handling.js
+++ frontend/src/utils/error-handling.js
@@ -1,9 +1,17 @@
 import useToaster from "@/composables/use-toaster"
+import router from "@/router/index"
 const { addErrorMessage } = useToaster()
 
 export const handleError = async (response, error) => {
-  // Do nothing if there is no error
+  // Do nothing if there is actually no error
   if (!error.value) {
+    return
+  }
+
+  // Redirect to a dedicated page in case of 404
+  // NOTE: we could handle this at back-end level too, if we'd need more granularity.
+  if (response.value.status == 404) {
+    router.replace({ name: "NotFound" })
     return
   }
 
@@ -11,13 +19,16 @@ export const handleError = async (response, error) => {
   // https://vuelidate-next.netlify.app/advanced_usage.html#config-with-composition-api
   const backErrorData = await response.value.json()
   if (backErrorData.display == "global") {
+    // show an error toast
     addErrorMessage(backErrorData.detail)
   } else if (backErrorData.display == "non_field") {
+    // return a non field error to be displayed on top of form
     return { non_field: backErrorData.detail }
   } else if (backErrorData.display == "field") {
+    // return a field error to be displayed on the field itself
     return { [backErrorData.fieldName]: backErrorData.detail }
   }
 
-  // NOTE FOR LATER: auto logout (in case of 401) could be handled here
-  // NOTE FOR LATER: timeout could be handled ehre
+  // TODO LATER: auto logout (in case of 401) could be handled here
+  // TODO LATER: timeout could be handled here too
 }
