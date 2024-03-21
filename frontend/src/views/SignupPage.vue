5b7cf29a82e13add0d1a04ca130a61a4338430d0
ddahan <david.dahan3@gmail.com>
2024-03-21 12:32:01 +0000

    Add backendErrors to signup

--- frontend/src/views/SignupPage.vue
+++ frontend/src/views/SignupPage.vue
@@ -1,85 +1,87 @@
 <template>
   <SingleItemWrapper>
     <h4>Se créer un compte</h4>
-    <p class="fr-hint-text">Sauf mention contraire, tous les champs sont obligatoires.</p>
-    <DsfrInputGroup :error-message="firstErrorMsg(v$, 'lastName')">
-      <DsfrInput
-        v-model="state.lastName"
-        label="Nom"
-        labelVisible
-        type="text"
-        spellcheck="false"
-        autocomplete="family-name"
-        autofocus
-      />
-    </DsfrInputGroup>
-    <DsfrInputGroup :error-message="firstErrorMsg(v$, 'firstName')">
-      <DsfrInput
-        v-model="state.firstName"
-        label="Prénom"
-        labelVisible
-        type="text"
-        spellcheck="false"
-        autocomplete="given-name"
-      />
-    </DsfrInputGroup>
-    <DsfrInputGroup :error-message="firstErrorMsg(v$, 'email')">
-      <DsfrInput
-        v-model="state.email"
-        label="Adresse e-mail"
-        labelVisible
-        type="email"
-        hint="Elle sera nécessaire pour valider la création de votre compte."
-        autocomplete="email"
-        spellcheck="false"
-      />
-    </DsfrInputGroup>
-    <DsfrInputGroup :error-message="firstErrorMsg(v$, 'username')">
-      <DsfrInput
-        v-model="state.username"
-        label="Identifiant"
-        labelVisible
-        type="text"
-        hint="Il servira à vous connecter au service. Minimum 8 caractères."
-        autocomplete="off"
-        spellcheck="false"
-      />
-    </DsfrInputGroup>
-    <DsfrInputGroup :error-message="firstErrorMsg(v$, 'password')">
-      <DsfrInput
-        v-model="state.password"
-        labelVisible
-        :type="showPassword ? 'text' : 'password'"
-        autocomplete="off"
-        spellcheck="false"
-      >
-        <template #label>
-          <div class="flex items-center justify-between">
-            <div>Mot de passe</div>
-            <DsfrButton
-              @click="showPassword = !showPassword"
-              :label="showPassword ? 'Cacher' : 'Afficher'"
-              :icon="showPassword ? 'ri-eye-off-line' : 'ri-eye-line'"
-              size="sm"
-              tertiary
-              noOutline
-            />
-          </div>
-        </template>
-      </DsfrInput>
-      <div class="mt-1 fr-hint-text flex flex-col">
-        <div>Votre mot de passe :</div>
-        <DsfrBadge
-          v-for="rule in passwordRules"
-          :key="rule"
-          :label="rule"
-          class="!block !lowercase !bg-transparent !pl-0"
-          small
-          type="info"
+    <FormWrapper :externalResults="$externalResults">
+      <p class="fr-hint-text">Sauf mention contraire, tous les champs sont obligatoires.</p>
+      <DsfrInputGroup :error-message="firstErrorMsg(v$, 'lastName')">
+        <DsfrInput
+          v-model="state.lastName"
+          label="Nom"
+          labelVisible
+          type="text"
+          spellcheck="false"
+          autocomplete="family-name"
+          autofocus
         />
-      </div>
-    </DsfrInputGroup>
-    <DsfrButton class="!block !w-full" :disabled="isFetching" label="Créer le compte" @click="submit" />
+      </DsfrInputGroup>
+      <DsfrInputGroup :error-message="firstErrorMsg(v$, 'firstName')">
+        <DsfrInput
+          v-model="state.firstName"
+          label="Prénom"
+          labelVisible
+          type="text"
+          spellcheck="false"
+          autocomplete="given-name"
+        />
+      </DsfrInputGroup>
+      <DsfrInputGroup :error-message="firstErrorMsg(v$, 'email')">
+        <DsfrInput
+          v-model="state.email"
+          label="Adresse e-mail"
+          labelVisible
+          type="email"
+          hint="Elle sera nécessaire pour valider la création de votre compte."
+          autocomplete="email"
+          spellcheck="false"
+        />
+      </DsfrInputGroup>
+      <DsfrInputGroup :error-message="firstErrorMsg(v$, 'username')">
+        <DsfrInput
+          v-model="state.username"
+          label="Identifiant"
+          labelVisible
+          type="text"
+          hint="Il servira à vous connecter au service. Minimum 8 caractères."
+          autocomplete="off"
+          spellcheck="false"
+        />
+      </DsfrInputGroup>
+      <DsfrInputGroup :error-message="firstErrorMsg(v$, 'password')">
+        <DsfrInput
+          v-model="state.password"
+          labelVisible
+          :type="showPassword ? 'text' : 'password'"
+          autocomplete="off"
+          spellcheck="false"
+        >
+          <template #label>
+            <div class="flex items-center justify-between">
+              <div>Mot de passe</div>
+              <DsfrButton
+                @click="showPassword = !showPassword"
+                :label="showPassword ? 'Cacher' : 'Afficher'"
+                :icon="showPassword ? 'ri-eye-off-line' : 'ri-eye-line'"
+                size="sm"
+                tertiary
+                noOutline
+              />
+            </div>
+          </template>
+        </DsfrInput>
+        <div class="mt-1 fr-hint-text flex flex-col">
+          <div>Votre mot de passe :</div>
+          <DsfrBadge
+            v-for="rule in passwordRules"
+            :key="rule"
+            :label="rule"
+            class="!block !lowercase !bg-transparent !pl-0"
+            small
+            type="info"
+          />
+        </div>
+      </DsfrInputGroup>
+      <DsfrButton class="!block !w-full" :disabled="isFetching" label="Créer le compte" @click="submit" />
+    </FormWrapper>
   </SingleItemWrapper>
 </template>
 
@@ -87,10 +89,12 @@
 import { ref } from "vue"
 import { useVuelidate } from "@vuelidate/core"
 import SingleItemWrapper from "@/components/SingleItemWrapper"
+import FormWrapper from "@/components/FormWrapper"
 import { errorRequiredField, errorRequiredEmail, firstErrorMsg } from "@/utils/forms"
 import { useFetch } from "@vueuse/core"
 import { headers } from "@/utils/data-fetching"
 import { useRouter } from "vue-router"
+import { handleError } from "@/utils/error-handling"
 
 const showPassword = ref(false)
 const passwordRules = [
@@ -116,7 +120,8 @@ const rules = {
   password: errorRequiredField, // let back-end specify other errors (length, rules)
 }
 
-const v$ = useVuelidate(rules, state)
+const $externalResults = ref({})
+const v$ = useVuelidate(rules, state, { $externalResults })
 
 // Request definition
 const { response, execute, isFetching } = useFetch(
@@ -130,15 +135,17 @@ const { response, execute, isFetching } = useFetch(
   .json()
 
 // Form validation
+
 const submit = async () => {
+  v$.value.$clearExternalResults()
   v$.value.$validate()
   if (v$.value.$error) {
     return // prevent API call if there is a front-end error
   }
   await execute()
-  // TODO HERE: wait for PR validation to handle back errors
-  // if (response.value.ok) {
-  //   useRouter().push({ name: "LoginPage" })
-  // }
+  $externalResults.value = await handleError(response)
+  if (response.value.ok) {
+    useRouter().push({ name: "LoginPage" })
+  }
 }
 </script>
