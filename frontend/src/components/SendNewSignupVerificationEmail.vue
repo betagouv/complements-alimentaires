bcdaa169b2a2aa549b2a257c27df9ad40f4122e9
ddahan <david.dahan3@gmail.com>
2024-03-27 15:16:46 +0000

    Add modal, extra data to error handling, and a dirty hack

--- frontend/src/components/SendNewSignupVerificationEmail.vue
+++ frontend/src/components/SendNewSignupVerificationEmail.vue
@@ -1,18 +1,19 @@
 <template>
   <div>
-    <DsfrButton v-if="!emailNotReceived" size="sm" label="Je n'ai pas reçu d'email" @click="emailNotReceived = true" />
-    <template v-if="emailNotReceived">
+    <DsfrButton size="sm" label="Je n'ai pas reçu d'email" @click="opened = true" ref="modalOrigin" />
+    <DsfrModal
+      :actions="actions"
+      ref="modal"
+      :opened="opened"
+      @close="close"
+      title="E-mail de vérification non reçu ?"
+      icon="ri-mail-forbid-line"
+    >
       <p>
         Si vous n'avez pas reçu d'email au bout de quelques minutes, veuillez vérifier l'adresse e-mail entrée, ainsi
         que vos courriers indésirables. Sinon, cliquez sur le bouton ci-dessous pour recevoir un nouvel e-mail :
       </p>
-      <DsfrButton
-        size="sm"
-        label="Renvoyer un e-mail de vérification"
-        :disabled="isFetching || isFinished"
-        @click="resendEmail"
-      />
-    </template>
+    </DsfrModal>
   </div>
 </template>
 
@@ -23,12 +24,13 @@ import { handleError } from "@/utils/error-handling"
 import useToaster from "@/composables/use-toaster"
 
 const props = defineProps({ userId: Number })
+const opened = ref(false)
 
-const emailNotReceived = ref(false)
+const close = () => (opened.value = false)
 
 // Main request definition
 const url = computed(() => `/api/v1/send-new-signup-verification-email/${props.userId}`)
-const { response, execute, isFetching, isFinished } = useFetch(url, {
+const { response, execute } = useFetch(url, {
   immediate: false,
 }).json()
 
@@ -38,5 +40,18 @@ const resendEmail = async () => {
   if (response.value.ok) {
     useToaster().addSuccessMessage("L'email de vérification a été renvoyé.")
   }
+  close()
 }
+
+const actions = [
+  {
+    label: "Renvoyer un nouvel e-mail",
+    onClick: resendEmail,
+  },
+  {
+    label: "Annuler",
+    onClick: close,
+    secondary: true,
+  },
+]
 </script>
