bcfdec60c29ff3280dc47de536fcb42c83351642
ddahan <david.dahan3@gmail.com>
2024-04-01 09:12:44 +0000

    Change tests on all test_user file

--- api/tests/test_user.py
+++ api/tests/test_user.py
@@ -1,8 +1,6 @@
 import json
-from django.urls import reverse
-from rest_framework.test import APITestCase
 from rest_framework import status
-from .utils import authenticate
+from .utils import ProjectAPITestCase
 from data.factories import DeclarantFactory, CompanySupervisorFactory
 from django.contrib.auth import get_user_model
 from data.factories.user import UserFactory
@@ -10,60 +8,64 @@ from data.factories.user import UserFactory
 User = get_user_model()
 
 
-class TestLoggedUserApi(APITestCase):
+class TestLoggedUserApi(ProjectAPITestCase):
+    viewname = "logged_user"
+
     def test_unauthenticated_logged_user_call(self):
         """
         When calling this API unathenticated we expect a 204
         """
-        response = self.client.get(reverse("api:logged_user"))
+        response = self.get(self.url())
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    @authenticate
     def test_authenticated_logged_user_call(self):
         """
         When calling this API authenticated we expect to get a
         JSON representation of the authenticated user
         """
-        response = self.client.get(reverse("api:logged_user"))
+        user = self.login()
+        response = self.get(self.url())
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         body = json.loads(response.content.decode())
-        self.assertEqual(body.get("id"), authenticate.user.id)
-        self.assertEqual(body.get("username"), authenticate.user.username)
-        self.assertEqual(body.get("email"), authenticate.user.email)
-        self.assertEqual(body.get("firstName"), authenticate.user.first_name)
-        self.assertEqual(body.get("lastName"), authenticate.user.last_name)
+        self.assertEqual(body.get("id"), user.id)
+        self.assertEqual(body.get("username"), user.username)
+        self.assertEqual(body.get("email"), user.email)
+        self.assertEqual(body.get("firstName"), user.first_name)
+        self.assertEqual(body.get("lastName"), user.last_name)
         self.assertEqual(body.get("roles"), [])
         self.assertIn("id", body)
 
-    @authenticate
     def test_authenticated_logged_user_call_with_roles(self):
         """Ensure that roles are added to the JSON representation of the user"""
 
         def _get_role_names(resp):
             return {role["name"] for role in resp.data["roles"]}
 
+        user = self.login()
+
         # Without role
-        response = self.client.get(reverse("api:logged_user"))
+        response = self.get(self.url())
         self.assertEqual(_get_role_names(response), set())
 
         # With two roles
-        DeclarantFactory(user=authenticate.user)
-        CompanySupervisorFactory(user=authenticate.user)
+        DeclarantFactory(user=user)
+        CompanySupervisorFactory(user=user)
 
-        response = self.client.get(reverse("api:logged_user"))
+        response = self.get(self.url())
         self.assertEqual(_get_role_names(response), {"Declarant", "CompanySupervisor"})
 
 
-class TestSignup(APITestCase):
+class TestSignup(ProjectAPITestCase):
+    viewname = "signup"
+
     def setUp(self):
         self.user_data = dict(
             last_name="Cook", first_name="Tim", email="tim.cook@exemple.com", username="tcook", password="azerty123$"
         )
-        self.url = reverse("api:signup")
 
     def test_signup_ok(self):
-        response = self.client.post(self.url, self.user_data)
+        response = self.post(self.url(), self.user_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertIn("user_id", response.data)
         new_user = User.objects.get(id=response.data["user_id"])
@@ -79,7 +81,7 @@ class TestSignup(APITestCase):
         UserFactory(email=email)
         self.user_data["email"] = email
 
-        response = self.client.post(self.url, self.user_data)
+        response = self.post(self.url(), self.user_data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("email", response.data["field_errors"])
 
@@ -88,82 +90,86 @@ class TestSignup(APITestCase):
         UserFactory(username=username)
         self.user_data["username"] = username
 
-        response = self.client.post(self.url, self.user_data)
+        response = self.post(self.url(), self.user_data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("username", response.data["field_errors"])
 
     def test_signup_with_invalid_password(self):
         self.user_data["password"] = "123"  # invalid
-        response = self.client.post(self.url, self.user_data)
+        response = self.post(self.url(), self.user_data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("password", response.data["field_errors"])
 
 
-class TestChangePassword(APITestCase):
+class TestChangePassword(ProjectAPITestCase):
+    viewname = "change_password"
+
     def setUp(self):
-        self.url = reverse("api:change_password")
         self.password = "azerty123$"
         self.user = UserFactory(password=self.password)
 
     def test_change_password_ok(self):
         new_password = "azerty321$"
-        self.client.force_login(self.user)
-        response = self.client.post(
-            self.url, dict(old_password=self.password, new_password=new_password, confirm_new_password=new_password)
+        self.login(self.user)
+        response = self.post(
+            self.url(), dict(old_password=self.password, new_password=new_password, confirm_new_password=new_password)
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_change_password_with_invalid_old_password(self):
         new_password = "azerty321$"
-        self.client.force_login(self.user)
-        response = self.client.post(
-            self.url, dict(old_password="wrong-pasword", new_password=new_password, confirm_new_password=new_password)
+        self.login(self.user)
+        response = self.post(
+            self.url(),
+            dict(old_password="wrong-pasword", new_password=new_password, confirm_new_password=new_password),
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("old_password", response.data["field_errors"])
 
     def test_change_password_with_identical_new_password(self):
-        self.client.force_login(self.user)
-        response = self.client.post(
-            self.url, dict(old_password=self.password, new_password=self.password, confirm_new_password=self.password)
+        self.login(self.user)
+        response = self.post(
+            self.url(),
+            dict(old_password=self.password, new_password=self.password, confirm_new_password=self.password),
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("new_password", response.data["field_errors"])
 
     def test_change_password_with_invalid_new_password(self):
         new_password = "a"
-        self.client.force_login(self.user)
-        response = self.client.post(
-            self.url, dict(old_password=self.password, new_password=new_password, confirm_new_password=new_password)
+        self.login(self.user)
+        response = self.post(
+            self.url(), dict(old_password=self.password, new_password=new_password, confirm_new_password=new_password)
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertTrue(response.data["non_field_errors"])  # Django default behaviour
 
     def test_change_password_with_wrong_confirm(self):
         new_password = "azerty321$"
-        self.client.force_login(self.user)
-        response = self.client.post(
-            self.url,
+        self.login(self.user)
+        response = self.post(
+            self.url(),
             dict(old_password=self.password, new_password=new_password, confirm_new_password=new_password + "."),
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("confirm_new_password", response.data["field_errors"])
 
     def test_change_password_unauthenticated(self):
-        response = self.client.post(self.url, {})
+        response = self.post(self.url(), {})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
-class TestEditUser(APITestCase):
+class TestEditUser(ProjectAPITestCase):
+    viewname = "edit_user"
+
     def setUp(self):
-        self.url = reverse("api:edit_user")
         self.user_data = dict(last_name="Cook", first_name="Tim", email="tim.cook@exemple.com", username="tcook")
         self.user = UserFactory(**self.user_data, is_verified=True)
 
     def test_edit_user_ok(self):
-        self.client.force_login(self.user)
+        self.login(self.user)
         new_last_name = "Cookie"
-        response = self.client.post(self.url, self.user_data | dict(last_name=new_last_name))
+        response = self.post(self.url(), self.user_data | dict(last_name=new_last_name))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["last_name"], new_last_name)
         self.user.refresh_from_db()
@@ -171,9 +177,9 @@ class TestEditUser(APITestCase):
         self.assertTrue(self.user.is_verified)  # user is still verified since email has not been changed
 
     def test_edit_user_with_email_changed_ok(self):
-        self.client.force_login(self.user)
+        self.login(self.user)
         new_email = "tim.cookie@exemple.com"
-        response = self.client.post(self.url, self.user_data | dict(email=new_email))
+        response = self.post(self.url(), self.user_data | dict(email=new_email))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["email"], new_email)
         self.user.refresh_from_db()
@@ -183,43 +189,46 @@ class TestEditUser(APITestCase):
     def test_edit_user_with_existing_email_ko(self):
         existing_email = "sundar.pichai@exemple.com"
         UserFactory(email=existing_email)
-        self.client.force_login(self.user)
-        response = self.client.post(self.url, self.user_data | dict(email=existing_email))
+        self.login(self.user)
+        response = self.post(self.url(), self.user_data | dict(email=existing_email))
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("email", response.data["field_errors"])
 
     def test_edit_user_with_missing_data_ko(self):
-        self.client.force_login(self.user)
-        response = self.client.post(self.url, {})
+        self.login(self.user)
+        response = self.post(self.url(), {})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         # NOTE: first_name and last_name are not mandatory at back-end level (should they?)
         self.assertEqual(len(response.data["field_errors"]), 2)
 
     def test_edit_user_unauthenticated(self):
-        response = self.client.post(self.url, {})
+        response = self.post(self.url(), {})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
-class TestDeleteUser(APITestCase):
+class TestDeleteUser(ProjectAPITestCase):
+    viewname = "delete_user"
+
     def setUp(self):
-        self.url = reverse("api:delete_user")
         self.user = UserFactory(is_verified=True)
 
     def test_delete_user_ok(self):
         self.assertTrue(self.user.is_active)
-        self.client.force_login(self.user)
-        response = self.client.delete(self.url)
+        self.login(self.user)
+        response = self.delete(self.url())
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.user.refresh_from_db()
         self.assertFalse(self.user.is_active)
 
     def test_delete_user_unauthenticated(self):
-        response = self.client.delete(self.url)
+        response = self.delete(self.url())
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
-class TestGenerateUsername(APITestCase):
+class TestGenerateUsername(ProjectAPITestCase):
+    viewname = "generate_username"
+
     def test_generate_username(self):
-        response = self.client.get(reverse("api:generate_username") + "?first_name=jean&last_name=dupon")
+        response = self.get(self.url() + "?first_name=jean&last_name=dupon")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, {"username": "jean.dupon"})
--- api/tests/utils.py
+++ api/tests/utils.py
@@ -1,12 +1,38 @@
-import functools
+from django.urls import reverse
+from django.core.exceptions import ImproperlyConfigured
 from data.factories import UserFactory
+from rest_framework.test import APITestCase
 
 
-def authenticate(func):
-    @functools.wraps(func)
-    def authenticate_and_func(*args, **kwargs):
-        authenticate.user = UserFactory.create()
-        args[0].client.force_login(user=authenticate.user)
-        return func(*args, **kwargs)
+class ProjectAPITestCase(APITestCase):
+    namespace = "api"
+    viewname = None
 
-    return authenticate_and_func
+    def url(self, *args, **kwargs):
+        if not self.viewname:
+            raise ImproperlyConfigured("Please provide a viewname attribute")
+        return reverse(f"{self.namespace}:{self.viewname}", *args, **kwargs)
+
+    def login(self, user=None):
+        """Login the provided user, or a new one if not provided. Return the user"""
+
+        if not user:
+            user = UserFactory.create()
+        self.client.force_login(user)
+        return user
+
+    # HTTP Method Sugar
+    def get(self, url, *args, **kwargs):
+        return self.client.get(url, *args, **kwargs)
+
+    def post(self, url, *args, **kwargs):
+        return self.client.post(url, *args, **kwargs)
+
+    def put(self, url, *args, **kwargs):
+        return self.client.put(url, *args, **kwargs)
+
+    def patch(self, url, *args, **kwargs):
+        return self.client.patch(url, *args, **kwargs)
+
+    def delete(self, url, *args, **kwargs):
+        return self.client.delete(url, *args, **kwargs)
