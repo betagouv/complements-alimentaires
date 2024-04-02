b21acf1a11432934b3f4d56820bd5282f8ec9883
Perrine Letellier <perrine.letellier@pm.me>
2024-04-02 16:42:43 +0200

    refactor CSVImporter for OOP and reuse in file_upload view

--- web/views/file_upload.py
+++ web/views/file_upload.py
@@ -1,12 +1,10 @@
-import csv
 import logging
 from django.apps import apps
 from django.views.generic import FormView
 from django.shortcuts import render
 
 from web.forms import FileUploadForm
-from data.csv_importer import _import_csv_to_model
-from data.exceptions import CSVFileError
+from data.csv_importer import CSVImporter
 
 logger = logging.getLogger(__name__)
 
@@ -28,29 +26,10 @@ class FileUploadView(FormView):
             return render(request, "upload_success.html", {"file_name": uploaded_file.name})
         return render(request, "upload_file.html", {"form": form})
 
-    def handle_uploaded_file(self, csv_file, file_type):
-        logger.info(f"J'ai bien reçu le fichier {csv_file.name} de type {file_type}")
-        # TODO enregistrer le fichier sur disque ?
-        # destination = open(f"some/file/{csv_file.name}", "wb+")
-        # for chunk in csv_file.chunks():
-        #     destination.write(chunk)
-        # destination.close()
-
-        # Importer le fichier en base
-        model = list(filter(lambda model: model.__name__ == "Ingredient", apps.get_models()))[0]
-        try:
-            csv_string = csv_file.read().decode("utf-8-sig")
-        except UnicodeDecodeError as e:
-            raise CSVFileError(f"'{csv_file.name}' n'est pas un fichier unicode.", e)
-        try:
-            csv_lines = csv_string.splitlines()
-            dialect = csv.Sniffer().sniff(csv_lines[0])
-        except csv.Error as e:
-            raise CSVFileError(f"'{csv_file.name}' n'est pas un fichier csv.", e)
-
-        csvreader = csv.DictReader(csv_lines, dialect=dialect)
-
-        nb_row, nb_created, updated_models = _import_csv_to_model(csvreader, csv_file.name, model, is_relation=False)
+    def _handle_uploaded_file(self, csv_file, file_type):
+        model = list(filter(lambda model: model.__name__ == file_type, apps.get_models()))[0]
+        csv_importer = CSVImporter(csv_file, model, is_relation=False)
+        nb_row, nb_created, updated_models = csv_importer.import_csv()
         logger.info(
             f"Import de {csv_file.name} dans le modèle {model.__name__} terminé : {nb_row} objets importés, {nb_created} objets créés."
         )
