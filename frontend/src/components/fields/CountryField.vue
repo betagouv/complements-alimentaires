934aa3610d0cd4a3e2f7531a83402216ce09bfe2
ddahan <david.dahan3@gmail.com>
2024-04-05 18:44:55 +0000

    country field tempo

---
+++ frontend/src/components/fields/CountryField.vue
@@ -0,0 +1,24 @@
+<!-- TODO: isoler ce composant car la logique est déliée du reste - voir nuxt mason -->
+
+<!-- <template>
+  <div></div>
+</template>
+
+<script setup>
+// Initial request for gettings countries (TODO: isolate in a component?)
+
+import { onMounted } from "vue"
+import { useFetch } from "@vueuse/core"
+import { handleError } from "@/utils/error-handling"
+
+const {
+  data: countryChoices,
+  response: countryResponse,
+  execute: executeGetCountries,
+} = useFetch("/api/v1/countries/", { immediate: false }).json()
+
+onMounted(async () => {
+  await executeGetCountries()
+  await handleError(countryResponse)
+})
+</script> -->
