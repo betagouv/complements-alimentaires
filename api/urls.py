300375ca4f6e58a049eb41f182cd5de224f6e368
Perrine Letellier <perrine.letellier@pm.me>
2024-04-05 14:10:25 +0200

    chore:add comments in api urls

--- api/urls.py
+++ api/urls.py
@@ -3,12 +3,14 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 import api.views as views
 
+# TODO spinal-casify old camelCase routes
 urlpatterns = {
     path("blogPosts/", views.BlogPostsView.as_view(), name="blog_posts_list"),
     path("blogPosts/<int:pk>", views.BlogPostView.as_view(), name="single_blog_post"),
     path("subscribeNewsletter/", views.SubscribeNewsletter.as_view(), name="subscribe_newsletter"),
     path("reportIssue/", views.ReportIssue.as_view(), name="report_issue"),
     path("webinars/", views.WebinarView.as_view(), name="webinar_list"),
+    # Elements
     path("search/", views.SearchView.as_view(), name="search"),
     path("plants/<int:pk>", views.PlantRetrieveView.as_view(), name="single_plant"),
     path("plantParts/", views.PlantPartListView.as_view(), name="plant_part_list"),
@@ -16,6 +18,7 @@ urlpatterns = {
     path("microorganisms/<int:pk>", views.MicroorganismRetrieveView.as_view(), name="single_microorganism"),
     path("substances/<int:pk>", views.SubstanceRetrieveView.as_view(), name="single_substance"),
     path("elements/autocomplete/", views.AutocompleteView.as_view(), name="substance_autocomplete"),
+    # References
     path("populations/", views.PopulationListView.as_view(), name="population_list"),
     path("conditions/", views.ConditionListView.as_view(), name="condition_list"),
     path("effects/", views.EffectListView.as_view(), name="effect_list"),
