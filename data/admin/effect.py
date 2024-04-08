54b9246ddbd2b80f200abc172d57e9961a470e69
Perrine Letellier <perrine.letellier@pm.me>
2024-04-08 19:34:55 +0200

    missing file

---
+++ data/admin/effect.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.contrib import admin
+from data.models import Effect
+
+
+class EffectForm(forms.ModelForm):
+    class Meta:
+        widgets = {
+            "name": forms.Textarea(attrs={"cols": 60, "rows": 1}),
+            "siccrf_name_en": forms.Textarea(attrs={"cols": 60, "rows": 1}),
+        }
+
+
+@admin.register(Effect)
+class EffectAdmin(admin.ModelAdmin):
+    form = EffectForm
+    fields = [
+        "name",
+        "ca_name",
+        "siccrf_name_en",
+        "is_obsolete",
+        "ca_is_obsolete",
+        "creation_date",
+        "modification_date",
+    ]
+    readonly_fields = [
+        "name",
+        "siccrf_name_en",
+        "is_obsolete",
+        "creation_date",
+        "modification_date",
+    ]
+    list_display = [
+        "name",
+        "modification_date",
+    ]
+    list_filter = ("is_obsolete",)
