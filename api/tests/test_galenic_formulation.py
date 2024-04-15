6656d0062ed8bcc101147af32cb1d1d0a699204e
Perrine Letellier <perrine.letellier@pm.me>
2024-04-15 09:18:30 +0200

    use ProjectAPITestCase to make the galenic formulation test simplee

--- api/tests/test_galenic_formulation.py
+++ api/tests/test_galenic_formulation.py
@@ -1,14 +1,17 @@
-from django.urls import reverse
-from rest_framework.test import APITestCase
 from rest_framework import status
+
+from .utils import ProjectAPITestCase
 from data.factories import GalenicFormulationFactory
 
 
-class TestGalenicFormulationApi(APITestCase):
+class TestGalenicFormulationApi(ProjectAPITestCase):
+    viewname = "galenic_formulation_list"
+
     def test_get_galenic_formulation_list(self):
         """
         The API should return all non obsolete galenicFormulation that are not missing data
         """
+
         complete_formulations = [
             GalenicFormulationFactory.create(missing_import_data=False, siccrf_is_obsolete=False, ca_is_obsolete=False)
             for i in range(3)
@@ -21,7 +24,7 @@ class TestGalenicFormulationApi(APITestCase):
             GalenicFormulationFactory.create(missing_import_data=True, siccrf_is_obsolete=True, ca_is_obsolete=True)
             for i in range(3)
         ]
-        response = self.client.get(reverse("api:galenic_formulation_list"))
+        response = self.get(self.url())
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         body = response.json()
 
