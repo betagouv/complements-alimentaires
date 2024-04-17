17b935a0a33332a2b3fafb48095f34680cc95d69
ddahan <david.dahan3@gmail.com>
2024-04-17 09:47:14 +0000

    Correction des tests et factories

--- data/factories/company.py
+++ data/factories/company.py
@@ -1,10 +1,11 @@
 import factory
 import faker
-from data.models.company import Company
+from data.models.company import Company, ActivityChoices
 from data.utils.string_utils import make_random_str
 from data.choices import CountryChoices
 import string
 import random
+from phonenumber_field.phonenumber import PhoneNumber
 
 
 def _make_siret() -> str:
@@ -19,17 +20,31 @@ def _make_country_code() -> str:
     return random.choice(list(CountryChoices))
 
 
+def _make_activities() -> list[str]:
+    nb_activities = random.randint(1, len(list(ActivityChoices)))
+    return random.sample(list(ActivityChoices), nb_activities)
+
+
+def _make_phone_number():
+    """On n'utilise pas Faker ou random ici car la lib PhoneNumber est très (trop ?) stricte sur la validation des numéros"""
+    return PhoneNumber.from_string("06 10 20 30 40", region="FR")
+
+
 class CompanyFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Company
 
-    social_name = factory.Faker("company")
-    commercial_name = factory.Faker("company")
+    social_name = factory.Faker("company", locale="FR")
+    commercial_name = factory.Faker("company", locale="FR")
     siret = factory.LazyFunction(_make_siret)
-    address = factory.Faker("street_address")
-    postal_code = factory.Faker("postcode")
-    city = factory.Faker("city")
+    address = factory.Faker("street_address", locale="FR")
+    postal_code = factory.Faker("postcode", locale="FR")
+    city = factory.Faker("city", locale="FR")
     country = factory.LazyFunction(_make_country_code)
+    activities = factory.LazyFunction(_make_activities)
+    # contact
+    phone_number = factory.LazyFunction(_make_phone_number)
+    email = factory.Faker("email", locale="FR")
 
 
 class CompanyWithSiretFactory(CompanyFactory):
