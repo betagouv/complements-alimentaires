61e48606b1fe23a30daa2dfc47dec58683c52e42
ddahan <david.dahan3@gmail.com>
2024-04-18 15:20:05 +0000

    minor fix on UI

--- frontend/src/views/CompanyFormPage/CreateCompany.vue
+++ frontend/src/views/CompanyFormPage/CreateCompany.vue
@@ -7,77 +7,77 @@
       <strong>{{ company.identifier }}</strong>
       n'est pas encore enregistrée dans notre base de données. Pour ce faire, veuillez vérifier ou compléter les
       informations ci-dessous. À l'issue, vous en deviendrez automatiquement son gestionnaire.
-      <FormWrapper class="mx-auto mt-8">
-        <DsfrFieldset legend="Informations administratives de l'entreprise">
-          <DsfrInputGroup :error-message="firstErrorMsg(v$, 'socialName')">
-            <DsfrInput v-model="state.socialName" label="Dénomination sociale" required labelVisible />
+    </DsfrAlert>
+    <FormWrapper class="mx-auto mt-8">
+      <DsfrFieldset legend="Informations administratives de l'entreprise">
+        <DsfrInputGroup :error-message="firstErrorMsg(v$, 'socialName')">
+          <DsfrInput v-model="state.socialName" label="Dénomination sociale" required labelVisible />
+        </DsfrInputGroup>
+        <DsfrInputGroup :error-message="firstErrorMsg(v$, 'commercialName')">
+          <DsfrInput v-model="state.commercialName" label="Nom commercial" required labelVisible />
+        </DsfrInputGroup>
+        <DsfrInputGroup :error-message="firstErrorMsg(v$, 'address')">
+          <DsfrInput v-model="state.address" label="Adresse" required labelVisible hint="Numéro et voie" />
+        </DsfrInputGroup>
+        <DsfrInputGroup :error-message="firstErrorMsg(v$, 'additionalDetails')">
+          <DsfrInput
+            v-model="state.additionalDetails"
+            label="Complément d’adresse (optionnel)"
+            labelVisible
+            hint="Bâtiment, immeuble, escalier et numéro d’appartement"
+          />
+        </DsfrInputGroup>
+        <div class="flex gap-x-4">
+          <DsfrInputGroup :error-message="firstErrorMsg(v$, 'postalCode')">
+            <DsfrInput v-model="state.postalCode" label="Code postal" required labelVisible />
           </DsfrInputGroup>
-          <DsfrInputGroup :error-message="firstErrorMsg(v$, 'commercialName')">
-            <DsfrInput v-model="state.commercialName" label="Nom commercial" required labelVisible />
-          </DsfrInputGroup>
-          <DsfrInputGroup :error-message="firstErrorMsg(v$, 'address')">
-            <DsfrInput v-model="state.address" label="Adresse" required labelVisible hint="Numéro et voie" />
-          </DsfrInputGroup>
-          <DsfrInputGroup :error-message="firstErrorMsg(v$, 'additionalDetails')">
+          <div class="grow">
+            <DsfrInputGroup :error-message="firstErrorMsg(v$, 'city')">
+              <DsfrInput v-model="state.city" label="Ville" required labelVisible />
+            </DsfrInputGroup>
+          </div>
+        </div>
+        <DsfrInputGroup :error-message="firstErrorMsg(v$, 'cedex')">
+          <DsfrInput v-model="state.cedex" label="Cedex (optionnel)" labelVisible />
+        </DsfrInputGroup>
+      </DsfrFieldset>
+
+      <DsfrFieldset
+        legend="Activités de l'entreprise"
+        hint="Veuillez cocher obligatoirement une ou plusieurs des six cases proposées correspondant au
+type d’activité exercée par le déclarant."
+      >
+        <DsfrCheckboxSet
+          class="max-w-3xl"
+          v-model="state.activities"
+          :options="allActivities"
+          :error-message="firstErrorMsg(v$, 'activities')"
+        />
+      </DsfrFieldset>
+      <DsfrFieldset
+        legend="Informations de contact"
+        hint="Veuillez transmettre les coordonnées d’une personne au sein de la société que la DGAL pourra être amenée à contacter en cas de nécessité ou pour des informations complémentaires."
+      >
+        <div class="grid gap-4 grid-cols-2">
+          <DsfrInputGroup :error-message="firstErrorMsg(v$, 'phoneNumber')">
             <DsfrInput
-              v-model="state.additionalDetails"
-              label="Complément d’adresse (optionnel)"
+              required
+              type="tel"
+              v-model="state.phoneNumber"
+              label="N° de téléphone de contact"
               labelVisible
-              hint="Bâtiment, immeuble, escalier et numéro d’appartement"
             />
           </DsfrInputGroup>
-          <div class="flex gap-x-4">
-            <DsfrInputGroup :error-message="firstErrorMsg(v$, 'postalCode')">
-              <DsfrInput v-model="state.postalCode" label="Code postal" required labelVisible />
-            </DsfrInputGroup>
-            <div class="grow">
-              <DsfrInputGroup :error-message="firstErrorMsg(v$, 'city')">
-                <DsfrInput v-model="state.city" label="Ville" required labelVisible />
-              </DsfrInputGroup>
-            </div>
-          </div>
-          <DsfrInputGroup :error-message="firstErrorMsg(v$, 'cedex')">
-            <DsfrInput v-model="state.cedex" label="Cedex (optionnel)" labelVisible />
+          <DsfrInputGroup :error-message="firstErrorMsg(v$, 'email')">
+            <DsfrInput required v-model="state.email" label="Adresse e-mail de contact" labelVisible />
           </DsfrInputGroup>
-        </DsfrFieldset>
-
-        <DsfrFieldset
-          legend="Activités de l'entreprise"
-          hint="Veuillez cocher obligatoirement une ou plusieurs des six cases proposées correspondant au
-type d’activité exercée par le déclarant."
-        >
-          <DsfrCheckboxSet
-            class="max-w-3xl"
-            v-model="state.activities"
-            :options="allActivities"
-            :error-message="firstErrorMsg(v$, 'activities')"
-          />
-        </DsfrFieldset>
-        <DsfrFieldset
-          legend="Informations de contact"
-          hint="Veuillez transmettre les coordonnées d’une personne au sein de la société que la DGAL pourra être amenée à contacter en cas de nécessité ou pour des informations complémentaires."
-        >
-          <div class="grid gap-4 grid-cols-2">
-            <DsfrInputGroup :error-message="firstErrorMsg(v$, 'phoneNumber')">
-              <DsfrInput
-                required
-                type="tel"
-                v-model="state.phoneNumber"
-                label="N° de téléphone de contact"
-                labelVisible
-              />
-            </DsfrInputGroup>
-            <DsfrInputGroup :error-message="firstErrorMsg(v$, 'email')">
-              <DsfrInput required v-model="state.email" label="Adresse e-mail de contact" labelVisible />
-            </DsfrInputGroup>
-            <DsfrInputGroup :error-message="firstErrorMsg(v$, 'website')">
-              <DsfrInput v-model="state.website" label="Site web de l'entreprise (optionnel)" labelVisible />
-            </DsfrInputGroup>
-          </div>
-        </DsfrFieldset>
-        <DsfrButton label="Enregistrer l'entreprise" @click="submitCompany" :disabled="isFetching" />
-      </FormWrapper>
-    </DsfrAlert>
+          <DsfrInputGroup :error-message="firstErrorMsg(v$, 'website')">
+            <DsfrInput v-model="state.website" label="Site web de l'entreprise (optionnel)" labelVisible />
+          </DsfrInputGroup>
+        </div>
+      </DsfrFieldset>
+      <DsfrButton label="Enregistrer l'entreprise" @click="submitCompany" :disabled="isFetching" />
+    </FormWrapper>
   </div>
 </template>
 
