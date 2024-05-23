48eb5f25a17d9d0f58794b9c951c0f5885578d56
ddahan <david.dahan3@gmail.com>
2024-05-23 13:20:28 +0000

    display items

--- frontend/src/views/CompanyPage.vue
+++ frontend/src/views/CompanyPage.vue
@@ -6,7 +6,6 @@
         <DsfrButton @click="isEditing = true" label="Modifier les informations" icon="ri-edit-line" size="sm" />
       </div>
     </div>
-    <p>Consultez et mettez à jour les données de votre entreprise</p>
     <div v-if="isEditing">
       <CompanyForm
         @responseReady="handleResponse"
@@ -18,13 +17,45 @@
       />
     </div>
     <div v-else>
-      <p>formulaire readonly</p>
+      <p class="font-bold">Informations administratives de l'entreprise</p>
+      <div class="grid grid-col-3 border-t border-gray-100">
+        <div class="divide-y divide-gray-100">
+          <div v-for="line in companyLines" :key="line.title" class="grid grid-cols-3 py-5 whitespace-pre-line">
+            <dt class="font-medium">{{ line.title }}</dt>
+            <dd class="text-gray-700">{{ line.val }}</dd>
+          </div>
+        </div>
+      </div>
+      <p class="font-bold mt-4">Activités de l'entreprise</p>
+      <div class="grid grid-col-3 border-t border-gray-100">
+        <div class="divide-y divide-gray-100">
+          <div class="grid grid-cols-3 py-5 whitespace-pre-line">
+            <dt class="font-medium">Activités enregistrées</dt>
+            <dd class="text-gray-700">
+              <ul class="m-0">
+                <li v-for="activity in company.activities" :key="activity">
+                  {{ activity.toLowerCase() }}
+                </li>
+              </ul>
+            </dd>
+          </div>
+        </div>
+      </div>
+      <p class="font-bold mt-4">Informations de contact</p>
+      <div class="grid grid-col-3 border-t border-gray-100">
+        <div class="divide-y divide-gray-100">
+          <div v-for="line in contactLines" :key="line.title" class="grid grid-cols-3 py-5 whitespace-pre-line">
+            <dt class="font-medium">{{ line.title }}</dt>
+            <dd class="text-gray-700">{{ line.val }}</dd>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue"
+import { ref, onMounted, computed } from "vue"
 import { useFetch } from "@vueuse/core"
 import { handleError } from "@/utils/error-handling"
 import { useRoute } from "vue-router"
@@ -37,6 +68,42 @@ import CompanyForm from "@/components/CompanyForm"
 const route = useRoute()
 const rootStore = useRootStore()
 
+// Lecture
+
+const displayableAddress = (company) => {
+  /* affichage propre d'une adresse complète */
+  const lines = [
+    company.address,
+    company.additionalDetails,
+    `${company.postalCode} ${company.city}`,
+    company.cedex,
+    company.countryLabel,
+  ]
+  return lines.filter((line) => line !== "").join("\n")
+}
+
+const companyLines = computed(() => {
+  return [
+    company.value.siret ? { title: "N° SIRET", val: company.value.siret } : null,
+    company.value.vat ? { title: "N° VAT", val: company.value.vat } : null,
+    { title: "Dénomination sociale", val: company.value.socialName },
+    { title: "Nom commercial", val: company.value.commercialName },
+    {
+      title: "Adresse",
+      val: displayableAddress(company.value),
+    },
+  ].filter((item) => item !== null)
+})
+
+const contactLines = computed(() => {
+  return [
+    { title: "N° de téléphone de contact", val: company.value.phoneNumber },
+    { title: "Adresse e-mail de contact", val: company.value.email },
+    { title: "Site web de l'entreprise", val: company.value.website || "-" },
+  ]
+})
+
+// Edition
 const isEditing = ref(false)
 const singleCompanyUrl = `/api/v1/companies/${route.params.id}`
 
