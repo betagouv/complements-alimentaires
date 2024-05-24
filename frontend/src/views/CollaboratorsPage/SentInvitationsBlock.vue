bfb89fa90b8c45afafc5c119f5a66216bd195264
ddahan <david.dahan3@gmail.com>
2024-05-24 09:32:54 +0000

    Refactoring using a holder component

--- frontend/src/views/CollaboratorsPage/SentInvitationsBlock.vue
+++ frontend/src/views/CollaboratorsPage/SentInvitationsBlock.vue
@@ -1,47 +1,24 @@
 <template>
-  <div v-if="solicitations">
-    <SectionTitle title="Invitations envoyées" icon="ri-chat-upload-line" />
-    <p>Visualisez les invitations envoyées par les co-gestionnaires à rejoindre votre entreprise.</p>
-    <div v-for="solicitation in solicitations" :key="solicitation.id">
-      <div class="flex items-center">
-        <v-icon class="size-4" name="ri-chat-upload-line" />
-
-        <div class="ml-2">
-          <div>{{ solicitation.senderName }}</div>
-          <div class="-mt-1.5">à {{ solicitation.recipientEmail }}</div>
-          <div class="text-xs">
-            {{ isoToPrettyDate(solicitation.creationDate, dateOptions) }}
-            à {{ isoToPrettyTime(solicitation.creationDate) }}
-          </div>
-        </div>
-
-        <div class="ml-2 md:ml-8 flex flex-col gap-y-1">
-          <div class="italic">{{ solicitation.description }}</div>
-        </div>
-      </div>
-      <hr class="mt-4 -mb-2 border" />
-    </div>
-    <div v-if="solicitations.length === 0">
-      <p class="italic">Il n'y a aucune invitation en cours.</p>
-    </div>
-  </div>
+  <SolicitationsHolder
+    v-if="solicitations"
+    title="Invitations envoyées"
+    icon="ri-chat-upload-line"
+    description="Visualisez les invitations envoyées par les co-gestionnaires à rejoindre votre entreprise."
+    :solicitations="solicitations"
+    emptyText="Il n'y a aucune invitation en cours"
+    :actions="[]"
+    showRecipientEmail
+  />
 </template>
 
 <script setup>
-import SectionTitle from "@/components/SectionTitle"
 import { useFetch } from "@vueuse/core"
 import { handleError } from "@/utils/error-handling"
 import { headers } from "@/utils/data-fetching"
 import { onMounted } from "vue"
-import { isoToPrettyDate, isoToPrettyTime } from "@/utils/date"
+import SolicitationsHolder from "./SolicitationsHolder"
 
 const props = defineProps({ companyId: Number })
-
-const dateOptions = {
-  weekday: "short",
-  month: "short",
-  day: "numeric",
-}
 
 const {
   data: solicitations,
@@ -58,8 +35,5 @@ const {
 onMounted(async () => {
   await execute()
   await handleError(response)
-  if (!solicitations.value) {
-    solicitations.value = []
-  }
 })
 </script>
---
+++ frontend/src/views/CollaboratorsPage/SolicitationsHolder.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <SectionTitle :title="title" :icon="icon" />
+    <p>{{ description }}</p>
+    <div v-for="solicitation in solicitations" :key="solicitation.id">
+      <div class="flex items-center">
+        <v-icon class="size-4" name="ri-chat-download-line" />
+
+        <div class="ml-2">
+          <div>{{ solicitation.senderName }}</div>
+          <div class="-mt-1.5" v-if="showRecipientEmail">à {{ solicitation.recipientEmail }}</div>
+          <div class="text-xs">
+            {{ isoToPrettyDate(solicitation.creationDate, dateOptions) }}
+            à {{ isoToPrettyTime(solicitation.creationDate) }}
+          </div>
+        </div>
+
+        <div class="ml-2 md:ml-8 flex flex-col gap-y-1">
+          <div class="italic">{{ solicitation.description }}</div>
+          <div v-if="actions.length > 0" class="flex gap-x-2">
+            <DsfrButton
+              v-for="action in actions"
+              :key="action.label"
+              :label="action.label"
+              :icon="action.icon"
+              :primary="action.primary"
+              :secondary="action.secondary"
+              size="sm"
+              @click="emit('process', solicitation.id, action.name)"
+            />
+          </div>
+        </div>
+      </div>
+      <hr class="mt-4 -mb-2 border" />
+    </div>
+    <div v-if="solicitations.length === 0">
+      <p class="italic">{{ emptyText }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import SectionTitle from "@/components/SectionTitle"
+import { isoToPrettyDate, isoToPrettyTime } from "@/utils/date"
+
+defineProps({
+  title: String,
+  icon: String,
+  description: String,
+  solicitations: Array,
+  emptyText: String,
+  actions: Array,
+  showRecipientEmail: { type: Boolean, default: false }, // spécifique
+})
+
+const emit = defineEmits(["process"])
+
+const dateOptions = {
+  weekday: "short",
+  month: "short",
+  day: "numeric",
+}
+</script>
