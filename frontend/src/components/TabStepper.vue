7f194d1038a87322d9ee7659dc232ef356ece056
Alejandro MG <alejandro@amguillen.dev>
2024-07-30 17:44:04 +0200

    Add tab stepper at the bottom of tab components

---
+++ frontend/src/components/TabStepper.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="border p-4 bg-gray-100 text-right">
+    <DsfrButton
+      secondary
+      v-if="selectedTabIndex > 0"
+      @click="emit('back')"
+      :label="`Revenir à l'onglet « ${titles[selectedTabIndex - 1].title} »`"
+    />
+    <DsfrButton
+      class="ml-4"
+      v-if="selectedTabIndex < titles.length - 1"
+      @click="emit('forward')"
+      :label="`Passer à l'onglet « ${titles[selectedTabIndex + 1].title} »`"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps(["titles", "selectedTabIndex"])
+const emit = defineEmits(["back", "forward"])
+</script>
