195be1cc0cf2221e3a53480dde9e10bf5a665a4c
Alejandro MG <alejandro@amguillen.dev>
2024-08-30 09:30:57 +0200

    Call backend article change endpoint from the UI

--- frontend/src/components/DeclarationSummary/ArticleInfoRow.vue
+++ frontend/src/components/DeclarationSummary/ArticleInfoRow.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="flex justify-between items-center border-2 p-2">
     <DsfrModal
-      :actions="[{ label: 'Valider' }]"
+      :actions="[{ label: 'Valider', onClick: changeArticle }]"
       @close="articleModalOpened = false"
       :opened="articleModalOpened"
       title="Changer l'article"
     >
       <p>Vous pouvez changer l'article de cette déclaration en le sélectionnant ci-dessous.</p>
       <DsfrInputGroup>
-        <DsfrRadioButtonSet v-model="payload.article" name="Article" legend="" :options="options"></DsfrRadioButtonSet>
+        <DsfrRadioButtonSet v-model="newArticle" name="Article" legend="" :options="options"></DsfrRadioButtonSet>
       </DsfrInputGroup>
     </DsfrModal>
 
@@ -24,8 +24,11 @@
 </template>
 
 <script setup>
-import { ref } from "vue"
+import { useFetch } from "@vueuse/core"
+import { headers } from "@/utils/data-fetching"
+import { ref, onMounted } from "vue"
 import { articleOptions } from "@/utils/mappings"
+import useToaster from "@/composables/use-toaster"
 const articleModalOpened = ref(false)
 
 const omitOptions = ["ART_17", "ART_15_WARNING"]
@@ -33,5 +36,29 @@ const options = articleOptions
   .filter((x) => omitOptions.indexOf(x.value) === -1)
   .map((x) => ({ value: x.value, label: x.text }))
 
+const newArticle = ref()
+onMounted(() => (newArticle.value = payload.value.article))
+
 const payload = defineModel()
+
+const changeArticle = async () => {
+  if (newArticle.value === payload.value.article) {
+    articleModalOpened.value = false
+    return
+  }
+  const url = `/api/v1/declarations/${payload.value.id}/update-article/`
+  const body = { article: newArticle.value }
+  const { response, data } = await useFetch(url, { headers: headers() }).post(body).json()
+  if (response.value.status >= 300 || response.value.status < 200) {
+    useToaster().addErrorMessage("Une erreur s'est produite, merci de ressayer plus tard")
+    return
+  }
+  payload.value = data.value
+  useToaster().addMessage({
+    type: "success",
+    id: "article-update-success",
+    description: "L'article de cette déclaration à été mis à jour",
+  })
+  articleModalOpened.value = false
+}
 </script>
