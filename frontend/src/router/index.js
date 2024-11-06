3583b2db7a8ed9c9c1bca6409c27b6fb4677086a
Helen Root <helenroot1@gmail.com>
2024-11-06 18:16:14 +0100

    Fix infinite redirection for logged in users of the wrong role

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -374,7 +374,9 @@ const chooseAuthorisedRoute = async (to, from, next, store) => {
       store.companies?.some((c) => c.roles?.some((x) => x.name === to.meta.requiredRole)) ||
       store.loggedUser?.globalRoles?.some((x) => x.name === to.meta.requiredRole)
 
-    authenticationCheck && roleCheck ? next() : next({ name: "LoginPage", query: { next: to.path } })
+    if (!authenticationCheck) next({ name: "LoginPage", query: { next: to.path } })
+    else if (!roleCheck) next({ name: "DashboardPage" })
+    else next()
   }
 }
 
