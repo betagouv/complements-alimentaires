008938d6256270a349661112e602fbd86c47ad25
Helen Root <helenroot1@gmail.com>
2024-11-06 18:28:45 +0100

    Header styling

--- frontend/src/views/ElementForm/index.vue
+++ frontend/src/views/ElementForm/index.vue
@@ -1,138 +1,148 @@
 <template>
-  <div class="fr-container mb-8">
-    <DsfrBreadcrumb class="mb-8" :links="breadcrumbLinks" />
+  <div class="mb-8">
+    <div class="bg-blue-france-950 py-1">
+      <div class="fr-container">
+        <DsfrBreadcrumb :links="breadcrumbLinks" />
 
-    <h1>Modification élément</h1>
+        <h1 class="-mt-6 mb-4">Modification élément</h1>
+      </div>
+    </div>
+    <div class="fr-container">
+      <p>
+        <v-icon :name="icon" />
+        {{ typeName }}
+      </p>
 
-    <p>
-      <v-icon :name="icon" />
-      {{ typeName }}
-    </p>
+      <!-- TODO: tabs -->
+      <FormWrapper class="mx-auto">
+        <DsfrFieldset legend="Identité de l’ingrédient" legendClass="fr-h4">
+          <!-- TODO: validation -->
+          <div class="flex gap-x-4 -mb-8">
+            <DsfrInputGroup>
+              <DsfrInput v-model="state.caName" :label="formForType.name.label" required labelVisible />
+            </DsfrInputGroup>
+            <DsfrInputGroup v-if="formForType.nameEn">
+              <DsfrInput v-model="state.caNameEn" :label="formForType.nameEn.label" labelVisible />
+            </DsfrInputGroup>
+            <DsfrInputGroup v-if="formForType.family">
+              <!-- Question: multiselect or single? -->
+              <DsfrSelect v-model="state.caFamily" label="Famille de la plante" :options="plantFamilies" required />
+            </DsfrInputGroup>
+            <DsfrInputGroup v-if="formForType.genre">
+              <DsfrInput v-model="state.genre" label="Genre" labelVisible />
+            </DsfrInputGroup>
+            <DsfrInputGroup v-if="formForType.ingredientType">
+              <!-- Question: multiselect or single? -->
+              <!-- Question: do we have this in the DB? -->
+              <DsfrSelect v-model="state.ingredientType" label="Type ingrédient" :options="ingredientTypes" required />
+            </DsfrInputGroup>
+            <!-- TODO: sort out row col wrapping for default -->
+            <DsfrInputGroup v-if="formForType.function">
+              <!-- Question: multiselect or single? -->
+              <!-- Question: do we have this in the DB? -->
+              <DsfrSelect v-model="state.function" label="Fonction de l'ingrédient" :options="functions" required />
+            </DsfrInputGroup>
+            <DsfrInputGroup v-if="formForType.substanceType">
+              <!-- Question: multiselect or single? -->
+              <!-- Question: do we have this in the DB? -->
+              <DsfrSelect v-model="state.substanceType" label="Type de substance" :options="substanceTypes" required />
+            </DsfrInputGroup>
 
-    <!-- TODO: tabs -->
-    <FormWrapper class="mx-auto">
-      <DsfrFieldset legend="Identité de l’ingrédient" legendClass="fr-h4">
-        <!-- TODO: validation -->
-        <div class="flex gap-x-4 -mb-8">
+            <DsfrToggleSwitch
+              v-model="state.authorised"
+              label="Autorisation de l’ingrédient"
+              activeText="Authorisé"
+              inactiveText="Non authorisé"
+              label-left
+              class="self-center"
+            />
+          </div>
+          <div class="flex gap-x-4 -mb-4">
+            <DsfrInputGroup v-if="formForType.einecsNumber">
+              <DsfrInput v-model="state.einecsNumber" label="Numéro EINECS" labelVisible />
+            </DsfrInputGroup>
+            <DsfrInputGroup v-if="formForType.casNumber">
+              <DsfrInput v-model="state.casNumber" label="Numéro CAS" labelVisible />
+            </DsfrInputGroup>
+            <DsfrInputGroup v-if="formForType.source">
+              <DsfrInput v-model="state.source" label="Source" labelVisible />
+            </DsfrInputGroup>
+            <DsfrInputGroup v-if="formForType.sourceEn">
+              <DsfrInput v-model="state.sourceEn" label="Source en anglais" labelVisible />
+            </DsfrInputGroup>
+          </div>
           <DsfrInputGroup>
-            <DsfrInput v-model="state.caName" :label="formForType.name.label" required labelVisible />
+            <DsfrInput v-model="state.description" label="Description" labelVisible />
           </DsfrInputGroup>
-          <DsfrInputGroup v-if="formForType.nameEn">
-            <DsfrInput v-model="state.caNameEn" :label="formForType.nameEn.label" labelVisible />
-          </DsfrInputGroup>
-          <DsfrInputGroup v-if="formForType.family">
-            <!-- Question: multiselect or single? -->
-            <DsfrSelect v-model="state.caFamily" label="Famille de la plante" :options="plantFamilies" required />
-          </DsfrInputGroup>
-          <DsfrInputGroup v-if="formForType.genre">
-            <DsfrInput v-model="state.genre" label="Genre" labelVisible />
-          </DsfrInputGroup>
-          <DsfrInputGroup v-if="formForType.ingredientType">
-            <!-- Question: multiselect or single? -->
-            <!-- Question: do we have this in the DB? -->
-            <DsfrSelect v-model="state.ingredientType" label="Type ingrédient" :options="ingredientTypes" required />
-          </DsfrInputGroup>
-          <!-- TODO: sort out row col wrapping for default -->
-          <DsfrInputGroup v-if="formForType.function">
-            <!-- Question: multiselect or single? -->
-            <!-- Question: do we have this in the DB? -->
-            <DsfrSelect v-model="state.function" label="Fonction de l'ingrédient" :options="functions" required />
-          </DsfrInputGroup>
-          <DsfrInputGroup v-if="formForType.substanceType">
-            <!-- Question: multiselect or single? -->
-            <!-- Question: do we have this in the DB? -->
-            <DsfrSelect v-model="state.substanceType" label="Type de substance" :options="substanceTypes" required />
-          </DsfrInputGroup>
-
-          <DsfrToggleSwitch
-            v-model="state.authorised"
-            label="Autorisation de l’ingrédient"
-            activeText="Authorisé"
-            inactiveText="Non authorisé"
-            label-left
-            class="self-center"
-          />
-        </div>
-        <div class="flex gap-x-4 -mb-4">
-          <DsfrInputGroup v-if="formForType.einecsNumber">
-            <DsfrInput v-model="state.einecsNumber" label="Numéro EINECS" labelVisible />
-          </DsfrInputGroup>
-          <DsfrInputGroup v-if="formForType.casNumber">
-            <DsfrInput v-model="state.casNumber" label="Numéro CAS" labelVisible />
-          </DsfrInputGroup>
-          <DsfrInputGroup v-if="formForType.source">
-            <DsfrInput v-model="state.source" label="Source" labelVisible />
-          </DsfrInputGroup>
-          <DsfrInputGroup v-if="formForType.sourceEn">
-            <DsfrInput v-model="state.sourceEn" label="Source en anglais" labelVisible />
-          </DsfrInputGroup>
-        </div>
-        <DsfrInputGroup>
-          <DsfrInput v-model="state.description" label="Description" labelVisible />
-        </DsfrInputGroup>
-        <DsfrFieldset legend="Synonymes" legendClass="fr-text--lg">
-          <ul>
-            <li v-for="(synonym, idx) in synonyms" :key="idx">{{ synonym.label }}, {{ synonym.type }}</li>
-          </ul>
-          <!-- TODO: table -->
-          <!-- TODO: delete/edit per line -->
-          <!-- TODO: add new line -->
-          <!-- TODO: if new, three ? editable lines by default -->
-          <DsfrButton label="Ajouter un synonyme" @click="addNewSynonym" icon="ri-add-line" size="sm" />
-        </DsfrFieldset>
-        <!-- name: {
+          <DsfrFieldset legend="Synonymes" legendClass="fr-text--lg">
+            <ul>
+              <li v-for="(synonym, idx) in synonyms" :key="idx">{{ synonym.label }}, {{ synonym.type }}</li>
+            </ul>
+            <!-- TODO: table -->
+            <!-- TODO: delete/edit per line -->
+            <!-- TODO: add new line -->
+            <!-- TODO: if new, three ? editable lines by default -->
+            <DsfrButton label="Ajouter un synonyme" @click="addNewSynonym" icon="ri-add-line" size="sm" />
+          </DsfrFieldset>
+          <!-- name: {
             label: "Nom de la plante",
           },
           family: true,
           function: true,
           // authorise: true for every type
           // description is true for every type -->
-      </DsfrFieldset>
-      <DsfrFieldset legend="Utilisation de l’ingrédient" legendClass="fr-h4">
-        <div v-if="formForType.usedParts" class="flex items-center my-4">
-          <DsfrMultiselect v-model="state.usedParts" :options="usedParts" label="Partie(s) utilisée(s)" search />
-          <div class="ml-4">
-            <DsfrTag v-for="id in state.usedParts" :key="id" :label="optionLabel(usedParts, id)" class="mx-1"></DsfrTag>
+        </DsfrFieldset>
+        <DsfrFieldset legend="Utilisation de l’ingrédient" legendClass="fr-h4">
+          <div v-if="formForType.usedParts" class="flex items-center my-4">
+            <DsfrMultiselect v-model="state.usedParts" :options="usedParts" label="Partie(s) utilisée(s)" search />
+            <div class="ml-4">
+              <DsfrTag
+                v-for="id in state.usedParts"
+                :key="id"
+                :label="optionLabel(usedParts, id)"
+                class="mx-1"
+              ></DsfrTag>
+            </div>
           </div>
-        </div>
-        <div v-if="formForType.activeSubstances" class="flex items-center my-4">
-          <!-- TODO: option to create new active substance -->
-          <DsfrMultiselect
-            v-model="state.activeSubstances"
-            :options="activeSubstances"
-            label="Substances actives"
-            search
-          />
-          <div class="ml-4">
-            <DsfrTag
-              v-for="id in state.activeSubstances"
-              :key="id"
-              :label="optionLabel(activeSubstances, id)"
-              class="mx-1"
-            ></DsfrTag>
-          </div>
-        </div>
-        <p><i>Population cible et à risque en construction</i></p>
-      </DsfrFieldset>
-      <DsfrFieldset legend="Commentaire à destination du public" legendClass="fr-h4">
-        <div class="flex">
-          <DsfrInput label="Commentaire" v-model="state.publicComment" :isTextarea="true" />
-          <div class="mt-2">
-            <DsfrCheckbox
-              v-model="state.copyPublicCommentToPrivate"
-              name="copy-comment"
-              label="Copier dans les notes internes"
+          <div v-if="formForType.activeSubstances" class="flex items-center my-4">
+            <!-- TODO: option to create new active substance -->
+            <DsfrMultiselect
+              v-model="state.activeSubstances"
+              :options="activeSubstances"
+              label="Substances actives"
+              search
             />
+            <div class="ml-4">
+              <DsfrTag
+                v-for="id in state.activeSubstances"
+                :key="id"
+                :label="optionLabel(activeSubstances, id)"
+                class="mx-1"
+              ></DsfrTag>
+            </div>
           </div>
+          <p><i>Population cible et à risque en construction</i></p>
+        </DsfrFieldset>
+        <DsfrFieldset legend="Commentaire à destination du public" legendClass="fr-h4">
+          <div class="flex">
+            <DsfrInput label="Commentaire" v-model="state.publicComment" :isTextarea="true" />
+            <div class="mt-2">
+              <DsfrCheckbox
+                v-model="state.copyPublicCommentToPrivate"
+                name="copy-comment"
+                label="Copier dans les notes internes"
+              />
+            </div>
+          </div>
+        </DsfrFieldset>
+        <div class="flex gap-x-2 mt-4">
+          <!-- Question: cancel button? -->
+          <!-- Question: use DsfrButtonGroup? -->
+          <DsfrButton label="Enregistrer ingrédient" @click="saveElement" :disabled="isFetching" />
+          <DsfrButton label="Sauvegarder brouillon" @click="saveAsDraft" :disabled="isFetching" secondary />
         </div>
-      </DsfrFieldset>
-      <div class="flex gap-x-2 mt-4">
-        <!-- Question: cancel button? -->
-        <!-- Question: use DsfrButtonGroup? -->
-        <DsfrButton label="Enregistrer ingrédient" @click="saveElement" :disabled="isFetching" />
-        <DsfrButton label="Sauvegarder brouillon" @click="saveAsDraft" :disabled="isFetching" secondary />
-      </div>
-    </FormWrapper>
+      </FormWrapper>
+    </div>
   </div>
 </template>
 
