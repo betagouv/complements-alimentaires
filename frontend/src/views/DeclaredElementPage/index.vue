1bd5d6989f5d5986e97047f271b5000177d7644d
Helen Root <helenroot1@gmail.com>
2024-11-20 18:42:17 +0100

    Front end modal for adding a note

--- frontend/src/views/DeclaredElementPage/index.vue
+++ frontend/src/views/DeclaredElementPage/index.vue
@@ -8,33 +8,44 @@
         description="Ingrédient non intégré dans la base de données et en attente de validation."
         class="mb-4"
       />
-      <div v-if="element" class="grid md:grid-cols-2 gap-4">
-        <div class="bg-grey-975 p-4 mb-8">
-          <p class="text-blue-france-sun-113">
-            <v-icon :name="icon" />
-            {{ typeName }}
-          </p>
-          <div class="grid grid-cols-3">
-            <p :aria-hidden="true" class="fr-h2">{{ authorizationInfo.flag }}</p>
-            <p class="content-center col-span-2">{{ authorizationInfo.text }}</p>
-          </div>
-          <div v-for="(info, idx) in elementProfile" :key="`element-profile-${idx}`" class="grid grid-cols-3">
-            <p>
-              <b>{{ info.label }}</b>
+      <div v-if="element">
+        <div class="grid md:grid-cols-2 gap-4">
+          <div class="bg-grey-975 p-4 mb-8">
+            <p class="text-blue-france-sun-113">
+              <v-icon :name="icon" />
+              {{ typeName }}
             </p>
-            <p class="col-span-2">
-              <a v-if="info.href" :href="info.href" target="_blank" rel="noopener" class="text-blue-france-sun-113">
-                {{ info.text }}
-              </a>
-              <span v-else>{{ info.text }}</span>
-            </p>
+            <div class="grid grid-cols-3">
+              <p :aria-hidden="true" class="fr-h2">{{ authorizationInfo.flag }}</p>
+              <p class="content-center col-span-2">{{ authorizationInfo.text }}</p>
+            </div>
+            <div v-for="(info, idx) in elementProfile" :key="`element-profile-${idx}`" class="grid grid-cols-3">
+              <p>
+                <b>{{ info.label }}</b>
+              </p>
+              <p class="col-span-2">
+                <a v-if="info.href" :href="info.href" target="_blank" rel="noopener" class="text-blue-france-sun-113">
+                  {{ info.text }}
+                </a>
+                <span v-else>{{ info.text }}</span>
+              </p>
+            </div>
+            <div class="grid justify-items-end p-2">
+              <router-link :to="declarationLink" class="text-blue-france-sun-113">
+                Voir la déclaration
+                <v-icon icon="ri-arrow-right-line"></v-icon>
+              </router-link>
+            </div>
           </div>
-          <div class="grid justify-items-end p-2">
-            <router-link :to="declarationLink" class="text-blue-france-sun-113">
-              Voir la déclaration
-              <v-icon icon="ri-arrow-right-line"></v-icon>
-            </router-link>
-          </div>
+        </div>
+        <div>
+          <DsfrButtonGroup :buttons="actionButtons" inlineLayoutWhen="md" align="center" class="mb-8" />
+
+          <DsfrModal :opened="!!modalToOpen" :title="title" :actions="actions" @close="closeModal">
+            <template #default>
+              <DsfrInput v-model="notes" label="Notes" label-visible is-textarea />
+            </template>
+          </DsfrModal>
         </div>
       </div>
     </div>
@@ -42,7 +53,7 @@
 </template>
 
 <script setup>
-import { computed, watch } from "vue"
+import { computed, watch, ref } from "vue"
 import { useFetch } from "@vueuse/core"
 import { getTypeIcon, getTypeInFrench, getApiType } from "@/utils/mappings"
 import { handleError } from "@/utils/error-handling"
@@ -138,5 +149,68 @@ watch(element, (newElement) => {
     const name = newElement.newName || `${newElement.newSpecies} ${newElement.newGenre}`
     document.title = `${name} - Compl'Alim`
   }
+})
+
+// Actions
+const actionButtons = [
+  {
+    label: "Demander plus d’information",
+    tertiary: true,
+    onclick: () => (modalToOpen.value = "info"),
+  },
+  {
+    label: "Refuser l’ingrédient",
+    tertiary: true,
+    "no-outline": true,
+    icon: "ri-close-line",
+    onclick: () => (modalToOpen.value = "refuse"),
+  },
+]
+
+const modalToOpen = ref(false)
+const closeModal = () => (modalToOpen.value = false)
+
+const modals = {
+  info: {
+    title: "L’ajout d’un nouvel ingrédient nécessite plus d’information.",
+    actions: [
+      {
+        label: "Enregistrer",
+        onClick() {
+          // TODO: update status and save note
+          closeModal()
+        },
+      },
+    ],
+  },
+  refuse: {
+    title: "L’ajout du nouvel ingrédient sera refusé.",
+    actions: [
+      {
+        label: "Refuser",
+        onClick() {
+          // TODO: update status and save note
+          closeModal()
+        },
+      },
+    ],
+  },
+}
+
+const title = computed(() => modals[modalToOpen.value]?.title)
+// TODO: prefill with existing note?
+const notes = ref()
+
+const actions = computed(() => {
+  const actions = modals[modalToOpen.value]?.actions || []
+  return actions.concat([
+    {
+      label: "Annuler",
+      secondary: true,
+      onClick() {
+        closeModal()
+      },
+    },
+  ])
 })
 </script>
