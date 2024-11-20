82eac26cc60f0e9fe5b88916a3026ccb50d2e1e5
Helen Root <helenroot1@gmail.com>
2024-11-20 11:53:44 +0100

    Remove unused filter code, to be added back in a different PR

--- frontend/src/views/NewElementsPage/index.vue
+++ frontend/src/views/NewElementsPage/index.vue
@@ -7,44 +7,6 @@
         :links="[{ to: { name: 'DashboardPage' }, text: 'Tableau de bord' }, { text: 'Ingrédients pour ajout' }]"
       />
       <h1 class="fr-h4">Liste des demandes en attente d’ajout d’ingrédients</h1>
-      <!-- <div class="border px-4 py-2 mb-2 md:flex gap-4 items-baseline filters">
-      <div class="flex gap-4">
-        <DsfrInputGroup>
-          <DsfrInput
-            class="!text-sm"
-            label="Nom"
-            :modelValue="name"
-            label-visible
-            @update:modelValue="updateNameFilter"
-          />
-        </DsfrInputGroup>
-        <DsfrInputGroup>
-          <DsfrSelect
-            label="Type"
-            :modelValue="type"
-            @update:modelValue="updateTypeFilter"
-            defaultUnselectedText=""
-            :options="typeSelectOptions"
-            class="!text-sm"
-          />
-        </DsfrInputGroup>
-      </div>
-      <div>
-        <div class="md:border-l md:pl-4 min-w-36 flex flex-row gap-4">
-          <DsfrInputGroup class="max-w-sm">
-            <DsfrSelect
-              label="Trier par"
-              defaultUnselectedText=""
-              :modelValue="ordering"
-              @update:modelValue="updateOrdering"
-              :options="orderingOptions"
-              class="!text-sm"
-            />
-          </DsfrInputGroup>
-          <PaginationSizeSelect :modelValue="limit" @update:modelValue="updateLimit" />
-        </div>
-      </div>
-    </div> -->
       <div v-if="isFetching" class="flex justify-center my-10">
         <ProgressSpinner />
       </div>
@@ -71,13 +33,9 @@ import ProgressSpinner from "@/components/ProgressSpinner"
 import NewElementsTable from "./NewElementsTable"
 import { useRoute, useRouter } from "vue-router"
 import { getPagesForPagination } from "@/utils/components"
-import { orderingOptions, typeOptions } from "@/utils/mappings"
-import PaginationSizeSelect from "@/components/PaginationSizeSelect"
 
 const router = useRouter()
 const route = useRoute()
-
-const typeSelectOptions = [...typeOptions, ...[{ value: "", text: "Tous" }]]
 
 const hasRequests = computed(() => data.value?.count > 0)
 const showPagination = computed(() => data.value?.count > data.value?.results?.length)
@@ -95,10 +53,6 @@ const limit = computed(() => route.query.limit)
 const updateQuery = (newQuery) => router.push({ query: { ...route.query, ...newQuery } })
 
 const updatePage = (newPage) => updateQuery({ page: newPage + 1 })
-// const updateNameFilter = (newValue) => updateQuery({ nom: newValue })
-// const updateTypeFilter = (newValue) => updateQuery({ type: newValue })
-// const updateOrdering = (newValue) => updateQuery({ triage: newValue })
-// const updateLimit = (newValue) => updateQuery({ limit: newValue, page: 1 })
 
 // Obtention de la donnée via API
 const url = computed(
@@ -113,15 +67,3 @@ const fetchSearchResults = async () => {
 
 watch([page, name, type, ordering, limit], fetchSearchResults)
 </script>
-
-<style scoped>
-.filters :deep(legend.fr-fieldset__legend) {
-  @apply pb-0 pt-4;
-}
-.filters :deep(.fr-input-group) {
-  @apply mb-0 mt-2;
-}
-.filters :deep(.fr-select-group) {
-  @apply mb-2;
-}
-</style>
