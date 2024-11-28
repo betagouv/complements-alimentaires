e10df66de1df2596b233dd88246719da97e502ea
qloridant <quentin.loridant@protonmail.com>
2024-11-28 13:43:10 +0100

    Separationd e la colonne company en deux colonnes :nom et siret

--- data/etl/transformer_loader.py
+++ data/etl/transformer_loader.py
@@ -45,4 +45,6 @@ class OPEN_DATA(TRANSFORMER_LOADER):
 class ETL_OPEN_DATA_DECLARATIONS(DECLARATIONS, OPEN_DATA):
     def transform_dataset(self):
         self.df = self.df.rename(columns=self.columns_mapper)
+        self.df["nom_fabriquant"] = self.df["nom_fabriquant"].apply(lambda x: x[0])
+        self.df["siret_fabriquant"] = self.df["nom_fabriquant"].apply(lambda x: x[1])
         self.clean_dataset()
