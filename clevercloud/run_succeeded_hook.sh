32a5afdf8b0977f751445b9c63baa478f214f410
Perrine Letellier <perrine.letellier@pm.me>
2024-12-16 11:28:58 +0100

    fix compare import date to execute only on import of each dump

--- clevercloud/run_succeeded_hook.sh
+++ clevercloud/run_succeeded_hook.sh
@@ -6,7 +6,8 @@ DATE=${FILEPATH##"s3://csv-data/"}
 DATE=${DATE%"/"}
 mkdir csv-data
 # if this date doesn't exist yet
-if ls csv-data/$DATE ; then
+LAST_DATE=$(psql postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME -t -1 -c "select * from data_last_import order by import_date desc limit 1;")
+if [[ "$LAST_DATE" == *"$DATE"* ]] ; then
     echo "This data has already been loaded"
 else
     # retrieve most recent files
@@ -14,4 +15,5 @@ else
     s3cmd get s3://csv-data/$DATE/ --recursive csv-data/$DATE/
     # import those files to database (django models)
     python ~/$APP_ID/manage.py load_ingredients -d csv-data/$DATE $DATE
+    psql postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME -c "insert into data_last_import (import_date) values ('$DATE');"
 fi
