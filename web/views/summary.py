a18221cf8a511bf33a6d23980620bbccf552fb43
Alejandro MG <alejandro@amguillen.dev>
2024-12-27 12:11:24 +0100

    Adds additional product and composition information

--- web/views/summary.py
+++ web/views/summary.py
@@ -13,6 +13,8 @@ from data.models import Declaration
 
 logger = logging.getLogger(__name__)
 
+OTHER_OPTION = "Autre (à préciser)"
+
 
 class SummaryView(GenericAPIView):
     permission_classes = [CanAccessIndividualDeclaration]
@@ -22,8 +24,6 @@ class SummaryView(GenericAPIView):
         declaration = self.get_object()
         template = get_template("summary.html")
         html = template.render(self.get_context(declaration))
-
-        # return HttpResponse(html)
 
         response = HttpResponse(content_type="application/pdf")
         filename = self.get_pdf_file_name(declaration)
@@ -44,13 +44,16 @@ class SummaryView(GenericAPIView):
             ("Description", declaration.description or "Non spécifiée"),
             ("Populations cibles", ", ".join(list(declaration.populations.all().values_list("name", flat=True)))),
             ("Populations à consommation déconseillée", self.get_conditions_string(declaration)),
-            ("Forme galénique", "TODO"),
+            (
+                "Forme galénique",
+                declaration.galenic_formulation or declaration.other_galenic_formulation or "Non spécifiée",
+            ),
             ("Mode d'emploi", declaration.instructions or "Non spécifié"),
-            ("Unité de consommation", "TODO"),
+            ("Unité de consommation", self.get_measurement_unit_string(declaration)),
             ("Dose journalière recommandée", declaration.daily_recommended_dose or "Non spécifiée"),
             ("Conditionnement", declaration.conditioning or "Non spécifié"),
             ("Durabilité minimale / DLUO (en mois)", declaration.minimum_duration or "Non spécifiée"),
-            ("Objectifs / effets", "TODO"),
+            ("Objectifs / effets", self.get_effects_string(declaration)),
         )
         return {
             "product_table_rows": product_table_rows,
@@ -64,15 +67,25 @@ class SummaryView(GenericAPIView):
         }
 
     def get_conditions_string(self, declaration):
+        has_other_conditions = declaration.conditions_not_recommended.filter(name=OTHER_OPTION).exists()
         conditions = ", ".join(
-            list(
-                declaration.conditions_not_recommended.exclude(name="Autre (à préciser)").values_list(
-                    "name", flat=True
-                )
-            )
+            list(declaration.conditions_not_recommended.exclude(name=OTHER_OPTION).values_list("name", flat=True))
         )
-        if declaration.other_conditions:
+        if has_other_conditions and declaration.other_conditions:
             return ", ".join([conditions, declaration.other_conditions])
+        return conditions
+
+    def get_effects_string(self, declaration):
+        has_other_effect = declaration.effects.filter(name=OTHER_OPTION).exists()
+        effects = ", ".join(list(declaration.effects.exclude(name=OTHER_OPTION).values_list("name", flat=True)))
+        if has_other_effect and declaration.other_effects:
+            return ", ".join([effects, declaration.other_effects])
+        return effects
+
+    def get_measurement_unit_string(self, declaration):
+        if not declaration.unit_quantity or not declaration.unit_measurement:
+            return "Non spécifiée"
+        return f"{declaration.unit_quantity} {declaration.unit_measurement.name}"
 
     def get_pdf_file_name(self, declaration):
         # Le load-balancer Sozu a du mal avec les noms de fichier pdf contenant des caractères non-ASCII
