0644f85918af33dfeb68fd296b453fbfb68bf64c
Perrine Letellier <perrine.letellier@pm.me>
2025-01-17 11:08:51 +0100

    add missing files

---
+++ frontend/src/components/DeclarationFromTeleicareAlert.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mb-4">
+    <DsfrAlert type="warning" class="mb-4" title="Cette déclaration est issue de l'historique Teleicare">
+      <p>L'import de l'historique est en cours. Les informations suivantes arriveront bientôt :</p>
+      <ul>
+        <li>la composition</li>
+        <li>les entreprises mandantes s'il y en a</li>
+        <li>l'étiquettage</li>
+        <li>l'attestation</li>
+      </ul>
+    </DsfrAlert>
+  </div>
+</template>
