cd394ec5d1c213f3e5f7c3ba0dda5c21d7aaa5c5
Alejandro MG <alejandro@amguillen.dev>
2025-01-21 19:26:21 +0100

    Usage de `del` pour a11y

--- frontend/src/views/VisaPage/VisaInfoLine.vue
+++ frontend/src/views/VisaPage/VisaInfoLine.vue
@@ -6,7 +6,9 @@
     </div>
     <div class="comment sm:flex-auto">
       <slot name="value">
-        <p class="mb-0 line-through" v-if="strikethroughText">{{ strikethroughText }}</p>
+        <p class="mb-0 line-through" v-if="strikethroughText">
+          <del>{{ strikethroughText }}</del>
+        </p>
         <p class="mb-0">{{ text }}</p>
       </slot>
     </div>
@@ -22,5 +24,23 @@ defineProps(["icon", "title", "text", "strikethroughText"])
 }
 .comment :deep(.fr-input-group) {
   @apply !mt-0;
+}
+del::before,
+del::after {
+  clip-path: inset(100%);
+  clip: rect(1px, 1px, 1px, 1px);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+}
+
+del::before {
+  content: " [début de la suppression] ";
+}
+
+del::after {
+  content: " [fin de la suppression] ";
 }
 </style>
