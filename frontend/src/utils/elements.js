ae72f94f429a71ede64e862f4411b1c8bb59a091
Helen Root <helenroot1@gmail.com>
2025-01-23 13:50:48 +0100

    Bug fix for adding a new microorganism to a declaration

--- frontend/src/utils/elements.js
+++ frontend/src/utils/elements.js
@@ -1,7 +1,10 @@
 import { slugifyType } from "@/utils/mappings"
 
 // on check e.substance car c'est le format des substances calculés
-export const getElementName = (e) => e.element?.name || e.substance?.name || e.newName
+// faut avoir quand même le format pour les microorganismes pour le cas où on travaille avec les données
+//  du front et non pas du back
+export const getElementName = (e) =>
+  e.element?.name || e.substance?.name || e.newName || `${e.newGenre} ${e.newSpecies}`
 
 export const getObjectSubTypeList = (objectList, subType = null) => {
   return subType
