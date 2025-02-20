f0cbdf22ceec921572ec06e65fab71bc7f93b801
Helen Root <helenroot1@gmail.com>
2025-02-20 20:23:25 +0100

    Bug fix: if ca field exists but not siccrf, still delete data

--- api/serializers/common_ingredient.py
+++ api/serializers/common_ingredient.py
@@ -58,7 +58,7 @@ class CommonIngredientModificationSerializer(serializers.ModelSerializer):
             if key.startswith("ca_"):
                 siccrf_key = key.replace("ca_", "siccrf_")
                 siccrf_value = getattr(instance, siccrf_key, None)
-                if not value and siccrf_value:
+                if not value and (siccrf_value or getattr(instance, key, None)):
                     validated_data[siccrf_key] = value  # mettre comme None ou "" selon la requête
                     logger.info(
                         f"SICCRF champ supprimé : le champ '{siccrf_key}' a été supprimé sur l'ingrédient type '{instance.object_type}', id '{instance.id}'"
