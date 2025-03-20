45185e1669b71df99cd39e3cca6d67c8c4b5e6df
Alejandro MG <alejandro@amguillen.dev>
2025-03-20 10:35:08 +0100

    Ajout de h1 et h2

--- frontend/src/views/AdvancedSearchResult/index.vue
+++ frontend/src/views/AdvancedSearchResult/index.vue
@@ -4,13 +4,18 @@
       :links="[
         { to: { name: 'DashboardPage' }, text: 'Tableau de bord' },
         { to: { name: 'AdvancedSearchPage' }, text: 'Recherche avancée' },
-        { text: 'Déclaration' },
+        { text: declaration?.name || 'Résultat' },
       ]"
     />
     <div v-if="isFetching" class="flex justify-center my-10">
       <ProgressSpinner />
     </div>
-    <DeclarationSummary :readonly="true" v-if="declaration" v-model="declaration" />
+    <div v-else>
+      <h1 class="fr-text--sm font-normal !mb-0">Résultat de la recherche</h1>
+      <h2>{{ declaration?.name }}</h2>
+
+      <DeclarationSummary :readonly="true" v-if="declaration" v-model="declaration" />
+    </div>
   </div>
 </template>
 
