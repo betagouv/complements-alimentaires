41b7d5997fbceacd2f02babb6e8ebca04e453b9c
Alejandro MG <alejandro@amguillen.dev>
2025-03-31 18:22:32 +0200

    Admin fixes

--- data/admin/error_report.py
+++ data/admin/error_report.py
@@ -8,4 +8,11 @@ class ErrorReportAdmin(admin.ModelAdmin):
     fields = ("email", "message", "author", "status", "element_string")
     readonly_fields = ("email", "message", "author", "element_string")
 
-    list_display = ("id", "message", "element_string", "status")
+    list_display = ("truncated_message", "element_string", "status")
+
+    def truncated_message(self, obj):
+        return f"{obj.message[:75]}..." if obj.message and len(obj.message) > 30 else obj.message
+
+    @admin.display(description="Ingrédient concerné")
+    def element_string(self, obj):
+        return obj.element_string
