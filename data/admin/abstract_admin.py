48eed6407b5efa5b6bf91457bc9b60cc6722723e
Helen Root <helenroot1@gmail.com>
2025-05-05 20:08:03 +0200

    Refactor declaration article recalculation into background task, add ONGOING_VISA status in filter

--- data/admin/abstract_admin.py
+++ data/admin/abstract_admin.py
@@ -1,5 +1,6 @@
 from django import forms
 
+from config import tasks
 from data.models import Declaration
 
 
@@ -30,15 +31,9 @@ class RecomputeDeclarationArticleAtIngredientSaveMixin:
         super().save_model(request, obj, form, change)
         # recalcul de l'article pour les déclarations concernées
         if change and form["is_risky"]._has_changed():
-            for declaration in Declaration.objects.filter(
-                id__in=getattr(obj, self.declaredingredient_set).values_list("declaration_id", flat=True),
-                status__in=(
-                    Declaration.DeclarationStatus.AWAITING_INSTRUCTION,
-                    Declaration.DeclarationStatus.ONGOING_INSTRUCTION,
-                    Declaration.DeclarationStatus.AWAITING_VISA,
-                    Declaration.DeclarationStatus.OBSERVATION,
-                    Declaration.DeclarationStatus.OBJECTION,
-                ),
-            ):
-                declaration.assign_calculated_article()
-                declaration.save()
+            ids = getattr(obj, self.declaredingredient_set).values_list("declaration_id", flat=True)
+            declarations_using_ingredient = Declaration.objects.filter(id__in=ids)
+            tasks.recalculate_article_for_ongoing_declarations(
+                declarations_using_ingredient,
+                f"Article recalculé après modification via l'admin de {obj.name} ({obj.object_type} id {obj.id})",
+            )
