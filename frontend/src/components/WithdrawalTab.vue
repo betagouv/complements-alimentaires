7588951467f55f4a5579298211aef80cdcf22a77
Alejandro MG <alejandro@amguillen.dev>
2025-05-06 17:31:38 +0200

    UI that asks for effective date

--- frontend/src/components/WithdrawalTab.vue
+++ frontend/src/components/WithdrawalTab.vue
@@ -5,7 +5,18 @@
       Votre produit « {{ declaration.name }} » a été autorisé. Vous pouvez néanmoins déclarer son arrêt de
       commercialisation en cliquant ci-dessous. Veuillez noter que cette opération est irreversible.
     </p>
-    <DsfrButton secondary label="Retirer ce complément" @click="confirmationModalOpened = true" />
+    <h3 class="fr-h5">Date effective du retrait du marché</h3>
+    <DsfrRadioButtonSet v-model="chosenDateOption" :options="dateOptions" label="Date effective de retrait du marché" />
+    <DsfrInputGroup v-if="chosenDateOption === 'other'" :error-message="firstErrorMsg(v$, 'effectiveDate')">
+      <DsfrInput
+        type="date"
+        label="Date effective de retrait du marché"
+        class="max-w-52 ml-8 mb-10 -mt-6"
+        v-model="otherDate"
+        required
+      />
+    </DsfrInputGroup>
+    <DsfrButton secondary label="Retirer ce complément" @click="openModal" />
 
     <DsfrModal title="Veuillez confirmer" :opened="confirmationModalOpened" @close="confirmationModalOpened = false">
       <p>Êtes-vous sûr de vouloir retirer ce produit du marché ?</p>
@@ -19,9 +30,12 @@
 
 <script setup>
 import { useFetch } from "@vueuse/core"
-import { ref } from "vue"
+import { ref, computed } from "vue"
 import { handleError } from "@/utils/error-handling"
 import { headers } from "@/utils/data-fetching"
+import { isoToPrettyDate } from "@/utils/date"
+import { useVuelidate } from "@vuelidate/core"
+import { errorRequiredField, firstErrorMsg } from "@/utils/forms"
 import useToaster from "@/composables/use-toaster"
 
 const declaration = defineModel()
@@ -38,5 +52,49 @@ const withdrawDeclaration = async () => {
     useToaster().addSuccessMessage("Votre produit a été retiré du marché")
     emit("withdraw")
   }
+}
+
+// Gestion de la date effective de retrait du marché
+const today = new Date()
+const dluoDate = computed(() => {
+  const months = parseInt(declaration.value?.minimumDuration)
+  if (!months) return
+  const afterDluo = new Date(today.getTime())
+  afterDluo.setMonth(afterDluo.getMonth() + months)
+  if (afterDluo.getDate() != today.getDate()) afterDluo.setDate(0)
+  return afterDluo
+})
+
+const chosenDateOption = ref(`${today}`)
+const otherDate = ref()
+const dateOptions = computed(() => {
+  const options = [
+    {
+      label: `Aujourd'hui (${isoToPrettyDate(today)})`,
+      value: `${today}`,
+    },
+  ]
+  if (dluoDate.value)
+    options.push({
+      label: `Aujourd'hui + DLUO (${isoToPrettyDate(dluoDate.value)})`,
+      hint: `Vous avez spécifié une durabilité minimale / DLUO de ${declaration.value.minimumDuration} mois`,
+      value: `${dluoDate.value}`,
+    })
+  options.push({
+    label: "Une autre date (à spécifier)",
+    value: "other",
+  })
+  return options
+})
+
+const effectiveDate = computed(() => (chosenDateOption.value === "other" ? otherDate.value : chosenDateOption.value))
+const rules = { effectiveDate: errorRequiredField }
+const v$ = useVuelidate(rules, { effectiveDate }, { $externalResults })
+
+const openModal = () => {
+  v$.value.$clearExternalResults()
+  v$.value.$validate()
+  if (v$.value.$error) return
+  confirmationModalOpened.value = true
 }
 </script>
