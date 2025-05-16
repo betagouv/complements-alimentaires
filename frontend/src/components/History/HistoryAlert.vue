1d7ca9510f55804431deeaae5fef302d299352f6
Perrine L. <1618987+pletelli@users.noreply.github.com>
2025-05-16 10:25:11 +0200

    Update frontend/src/components/History/HistoryAlert.vue
    Co-authored-by: Alejandro M Guillén <alejandro@amguillen.dev>

--- frontend/src/components/History/HistoryAlert.vue
+++ frontend/src/components/History/HistoryAlert.vue
@@ -3,7 +3,7 @@
     <DsfrAlert
       title="Historique des déclarations soumises sur Teleicare"
       description="Les produits que vous avez déjà déclarés sur Téléicare ont été importés grâce à une correspondance basée sur les SIRET et les numéros de TVA intracommunautaire.
-        L'étiquetage n'est pas disponibles dans cet historique."
+        L'étiquetage n'est pas disponible dans cet historique."
       type="warning"
     ></DsfrAlert>
   </div>
