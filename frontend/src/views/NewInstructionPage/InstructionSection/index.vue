9d60d78c4c31c949c09424dfe32e01f8f038378b
Alejandro MG <alejandro@amguillen.dev>
2025-06-10 16:12:11 +0200

    Cache le bouton d'affichage du json lors que l'action ne modifie pas la déclaration

--- frontend/src/views/NewInstructionPage/InstructionSection/index.vue
+++ frontend/src/views/NewInstructionPage/InstructionSection/index.vue
@@ -4,7 +4,7 @@
       <h2 id="pieces-jointes">Pièces jointes</h2>
       <CompactAttachmentGrid :attachments="declaration.attachments" />
       <h2 id="dernier-commentaire">Dernier commentaire</h2>
-      <LastComment class="mb-4" :snapshot="lastSnapshot" />
+      <LastComment class="mb-6" :snapshot="lastSnapshot" />
       <h2 id="composition-produit">Composition produit</h2>
       <DsfrAlert small description="Ce segment est en construction" class="mb-6" />
       <h2 id="resultat-instruction">Résultat de l'instruction</h2>
