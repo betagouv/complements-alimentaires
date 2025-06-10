b072016a1bb24040d00e1a756b2d1dd1ec10dadd
Alejandro MG <alejandro@amguillen.dev>
2025-06-10 14:27:41 +0200

    Ajout des SnapshotItems dans les éléments de l'historique

--- frontend/src/views/NewInstructionPage/HistorySection/index.vue
+++ frontend/src/views/NewInstructionPage/HistorySection/index.vue
@@ -1,10 +1,32 @@
 <template>
-  <div class="bg-grey-975! p-6">
+  <div class="bg-grey-975! p-6" v-if="snapshots">
     <h2 id="declarant-e">Historique</h2>
-    <DsfrAlert small description="Ce segment est en construction" class="mb-6" />
+
+    <div v-if="!snapshots" class="flex justify-center items-center min-h-60">
+      <ProgressSpinner />
+    </div>
+    <div v-if="snapshots && snapshots.length" class="flex flex-col gap-6">
+      <SnapshotItem
+        v-for="snapshot in snapshots"
+        :key="`snapshot-${snapshot.id}`"
+        :snapshot="snapshot"
+        :rightSide="showOnRight(snapshot)"
+      />
+    </div>
+    <div v-else>
+      <p>Nous n'avons pas l'historique pour cette déclaration.</p>
+    </div>
   </div>
 </template>
 
 <script setup>
-defineProps({ snapshots: Object })
+import ProgressSpinner from "@/components/ProgressSpinner"
+import SnapshotItem from "@/components/SnapshotItem"
+
+defineProps({ snapshots: Array })
+
+const showOnRight = (snapshot) => {
+  const rightSideStatus = ["OBSERVATION", "AUTHORIZED", "AWAITING_VISA", "OBJECTION"]
+  return rightSideStatus.indexOf(snapshot.status) > -1
+}
 </script>
---
+++ frontend/src/views/NewInstructionPage/InstructionSection/LastComment.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="snapshot">
+    <div class="p-4 bg-white rounded mb-4">
+      <SnapshotItem :snapshot="snapshot" />
+    </div>
+
+    <DsfrButton @click="goToHistory" label="Voir l'historique" secondary size="small" />
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from "vue-router"
+import SnapshotItem from "@/components/SnapshotItem"
+
+const route = useRoute()
+const router = useRouter()
+defineProps({ snapshot: Object })
+
+const goToHistory = () => router.push({ name: "HistorySection", params: route.params })
+</script>
