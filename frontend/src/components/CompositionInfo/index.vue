5e6767e835dac50590f3f224cc3bc2ceb540a6ef
Alejandro MG <alejandro@amguillen.dev>
2025-06-12 17:36:42 +0200

    Refactoring

--- frontend/src/components/CompositionInfo/index.vue
+++ frontend/src/components/CompositionInfo/index.vue
@@ -3,50 +3,10 @@
     <SummaryElementList
       :useAccordions="useAccordions"
       :showElementAuthorization="showElementAuthorization"
-      objectType="plant"
-      :elements="payload.declaredPlants"
-    />
-    <SummaryElementList
-      :useAccordions="useAccordions"
-      :showElementAuthorization="showElementAuthorization"
-      objectType="microorganism"
-      :elements="payload.declaredMicroorganisms"
-    />
-    <SummaryElementList
-      objectType="form_of_supply"
-      :useAccordions="useAccordions"
-      :showElementAuthorization="showElementAuthorization"
-      :elements="getObjectSubTypeList(payload.declaredIngredients, 'form_of_supply')"
-    />
-    <SummaryElementList
-      :useAccordions="useAccordions"
-      :showElementAuthorization="showElementAuthorization"
-      objectType="aroma"
-      :elements="getObjectSubTypeList(payload.declaredIngredients, 'aroma')"
-    />
-    <SummaryElementList
-      objectType="additive"
-      :useAccordions="useAccordions"
-      :showElementAuthorization="showElementAuthorization"
-      :elements="getObjectSubTypeList(payload.declaredIngredients, 'additive')"
-    />
-    <SummaryElementList
-      objectType="active_ingredient"
-      :useAccordions="useAccordions"
-      :showElementAuthorization="showElementAuthorization"
-      :elements="getObjectSubTypeList(payload.declaredIngredients, 'active_ingredient')"
-    />
-    <SummaryElementList
-      objectType="non_active_ingredient"
-      :useAccordions="useAccordions"
-      :showElementAuthorization="showElementAuthorization"
-      :elements="getObjectSubTypeList(payload.declaredIngredients, 'non_active_ingredient')"
-    />
-    <SummaryElementList
-      objectType="substance"
-      :useAccordions="useAccordions"
-      :showElementAuthorization="showElementAuthorization"
-      :elements="payload.declaredSubstances"
+      :objectType="section.objectType"
+      :elements="section.elements"
+      v-for="section in sections"
+      :key="section.objectType"
     />
   </div>
 </template>
@@ -54,8 +14,44 @@
 <script setup>
 import { getObjectSubTypeList } from "@/utils/elements"
 import SummaryElementList from "./SummaryElementList"
+import { computed } from "vue"
 
 const payload = defineModel()
 
 defineProps({ useAccordions: Boolean, showElementAuthorization: Boolean })
+
+const sections = computed(() => [
+  {
+    elements: payload.value.declaredPlants,
+    objectType: "plant",
+  },
+  {
+    elements: payload.value.declaredMicroorganisms,
+    objectType: "microorganism",
+  },
+  {
+    elements: getObjectSubTypeList(payload.value.declaredIngredients, "form_of_supply"),
+    objectType: "form_of_supply",
+  },
+  {
+    elements: getObjectSubTypeList(payload.value.declaredIngredients, "aroma"),
+    objectType: "aroma",
+  },
+  {
+    elements: getObjectSubTypeList(payload.value.declaredIngredients, "additive"),
+    objectType: "additive",
+  },
+  {
+    elements: getObjectSubTypeList(payload.value.declaredIngredients, "active_ingredient"),
+    objectType: "active_ingredient",
+  },
+  {
+    elements: getObjectSubTypeList(payload.value.declaredIngredients, "non_active_ingredient"),
+    objectType: "non_active_ingredient",
+  },
+  {
+    elements: payload.value.declaredSubstances,
+    objectType: "substance",
+  },
+])
 </script>
