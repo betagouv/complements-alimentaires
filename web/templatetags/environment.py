e79bfcdb5aafc300d032b9be03cfc5244006847c
Helen Root <helenroot1@gmail.com>
2025-06-23 20:46:55 +0200

    Make observation days overridable with an environment variable

--- web/templatetags/environment.py
+++ web/templatetags/environment.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.simple_tag
 def environment():
     return getattr(settings, "ENVIRONMENT", "")
+
+
+@register.simple_tag
+def observation_days():
+    return getattr(settings, "OBSERVATION_DAYS", "")
