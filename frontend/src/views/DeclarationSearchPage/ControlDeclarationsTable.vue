e11dbe6fae170d7345a851d1b1dd835a46e57ad5
Alejandro MG <alejandro@amguillen.dev>
2025-06-25 10:52:49 +0200

    Adds filter and sort icons in table headers

--- frontend/src/views/DeclarationSearchPage/ControlDeclarationsTable.vue
+++ frontend/src/views/DeclarationSearchPage/ControlDeclarationsTable.vue
@@ -1,20 +1,50 @@
 <template>
-  <DsfrTable
-    ref="table"
-    title="Toutes les déclarations"
-    :headers="headers"
-    :rows="rows"
-    :no-caption="true"
-    :pagination="false"
-  />
+  <DsfrTable ref="table" title="Toutes les déclarations" :rows="rows" :no-caption="true" :pagination="false">
+    <template #header>
+      <!-- Il faut générer programmatiquement les entêtes à cause du bug : https://github.com/dnum-mi/vue-dsfr/issues/1091 -->
+      <tr>
+        <DsfrTableHeader
+          :header="header.text"
+          :header-attrs="header.headerAttrs"
+          :icon="header.icon"
+          v-for="header in headers"
+          :key="header.text"
+        />
+      </tr>
+    </template>
+  </DsfrTable>
 </template>
 
 <script setup>
-import { computed } from "vue"
+import { computed, ref } from "vue"
 import { isoToPrettyDate } from "@/utils/date"
+import { getStatusTagForCell } from "./utils"
 
 const props = defineProps({ data: { type: Object, default: () => {} } })
-const headers = ["ID décla.", "Produit", "Entreprise", "Marque", "Statut du produit", "Date d'application du statut"]
+const headers = computed(() => [
+  { text: "ID décla." },
+  { text: "Produit", headerAttrs: getSortHeaderAttrs("name"), icon: getSortIcon("name") },
+  { text: "Entreprise", headerAttrs: getSortHeaderAttrs("company"), icon: getSortIcon("company") },
+  { text: "Marque", headerAttrs: getFilterHeaderAttrs("brand"), icon: "ri-filter-line" },
+  { text: "Statut du produit", headerAttrs: getFilterHeaderAttrs("status"), icon: "ri-filter-line" },
+  { text: "Date d'application du statut" },
+])
+
+// Gestion du triage
+const currentSort = ref()
+const sortDirection = ref()
+const sortBy = (sortParam) => console.log(`Sort by ${sortParam}`)
+const getSortHeaderAttrs = (sortParam) => ({ class: "cursor-pointer", onClick: () => sortBy(sortParam) })
+const getSortIcon = (sortParam) =>
+  currentSort.value === sortParam
+    ? sortDirection.value === "+"
+      ? "ri-sort-asc"
+      : "ri-sort-desc"
+    : "ri-arrow-up-down-line"
+
+// Gestion du filtrage
+const getFilterHeaderAttrs = (filterParam) => ({ class: "cursor-pointer", onClick: () => filterBy(filterParam) })
+const filterBy = (filterParam, filterValue) => console.log(`Filter by ${filterParam} : ${filterValue}`)
 
 const rows = computed(() =>
   props.data?.results?.map((x) => ({
@@ -24,7 +54,7 @@ const rows = computed(() =>
       { component: "p", text: x.name, class: "font-bold" },
       x.companyName,
       x.brand,
-      x.simplifiedStatus,
+      getStatusTagForCell(x.simplifiedStatus),
       x.simplifiedStatusDate ? isoToPrettyDate(x.simplifiedStatusDate) : "",
     ],
   }))
@@ -36,5 +66,8 @@ const rows = computed(() =>
 
 .fr-table :deep(table) {
   @apply table!;
+}
+.fr-table :deep(th .vicon) {
+  @apply ml-2!;
 }
 </style>
