64a91c0bff5273be52c1c8f5e9f3c8e43192e6fa
Alejandro MG <alejandro@amguillen.dev>
2025-07-15 20:08:51 +0200

    Allow opening the company page from the declaration one

--- frontend/src/views/DeclarationIndividualPage/CompanyControlSection.vue
+++ frontend/src/views/DeclarationIndividualPage/CompanyControlSection.vue
@@ -4,6 +4,8 @@
     <DeclarantIdentity class="mb-8" :user="declarant" />
     <SectionHeader id="entreprise" text="Identité de l’entreprise" icon="ri-building-4-fill" />
     <CompanyIdentity :company="company" :mandatedCompany="mandatedCompany" />
+
+    <DsfrButton class="mt-4" label="Voir tous les produits de cette entreprise" @click="openCompanyPage" />
   </div>
 </template>
 
@@ -11,6 +13,9 @@
 import SectionHeader from "@/components/NewBepiasViews/SectionHeader"
 import DeclarantIdentity from "@/components/DeclarantIdentity"
 import CompanyIdentity from "@/components/CompanyIdentity"
+import { useRouter } from "vue-router"
 
-defineProps({ declarant: Object, company: Object, mandatedCompany: Object })
+const props = defineProps({ declarant: Object, company: Object, mandatedCompany: Object })
+const router = useRouter()
+const openCompanyPage = () => router.push({ name: "CompanyDetails", params: { companyId: props.company.id } })
 </script>
