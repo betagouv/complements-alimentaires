dfb8f419b3af45e18fdb3535f5735f12eab28b0a
Perrine Letellier <perrine.letellier@pm.me>
2025-09-30 15:56:00 +0200

    add synonym type to ingredient interface

--- api/serializers/plant.py
+++ api/serializers/plant.py
@@ -77,7 +77,10 @@ class PlantSerializer(CommonIngredientReadSerializer):
 class PlantSynonymModificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlantSynonym
-        fields = ("name",)
+        fields = (
+            "name",
+            "synonym_type",
+        )
 
 
 class PlantPartModificationSerializer(serializers.ModelSerializer):
