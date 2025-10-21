e4a9c13a41653ca1a1f534a687e7c56a3c329bbf
Alejandro MG <alejandro@amguillen.dev>
2025-10-21 09:25:11 +0200

    Missing commit

--- data/admin/global_roles.py
+++ data/admin/global_roles.py
@@ -1,6 +1,10 @@
+import logging
+
 from django.contrib import admin
 
 from data.models import ControlRole, InstructionRole, VisaRole
+
+logger = logging.getLogger(__name__)
 
 
 @admin.register(InstructionRole)
@@ -15,7 +19,8 @@ class VisaRoleAdmin(admin.ModelAdmin):
 
 @admin.register(ControlRole)
 class ControlRoleAdmin(admin.ModelAdmin):
-    list_display = ("user", "persistant")
+    list_filter = ["always_persist"]
+    list_display = ("user", "persistant", "domaine_email_ok")
     search_fields = (
         "user__first_name",
         "user__last_name",
@@ -24,3 +29,7 @@ class ControlRoleAdmin(admin.ModelAdmin):
 
     def persistant(self, obj):
         return "🔒 Reste même si absent sur Grist" if obj.always_persist else " "
+
+    def domaine_email_ok(self, obj):
+        government_email = obj.user.email[-8:] in ["@gouv.fr", ".gouv.fr"]
+        return "" if government_email else f"⚠️ Adresse en « {obj.user.email.split('@')[-1]} »"
