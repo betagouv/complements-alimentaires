0e481a17414b5900242dae8997973b9666b3f22c
Alejandro MG <alejandro@amguillen.dev>
2025-11-12 17:50:08 +0100

    Changement de wording pour le badge des ingrédients autorisés

--- frontend/src/components/ElementStatusBadge.vue
+++ frontend/src/components/ElementStatusBadge.vue
@@ -1,5 +1,6 @@
 <template>
-  <DsfrBadge v-if="text === 'autorisé'" :label="text" small type="success" />
+  <!-- Nous n'utilisons pas le mot "autorisé" pour ne pas donner une fausse idée d'acceptation sure de l'ingrédient -->
+  <DsfrBadge v-if="text === 'autorisé'" label="déclarable" small type="info" />
   <DsfrBadge v-else-if="text === 'non autorisé'" :label="text" small type="error" />
   <DsfrBadge v-else-if="text === 'retiré par l\'administration'" :label="text" small type="error" />
 </template>
