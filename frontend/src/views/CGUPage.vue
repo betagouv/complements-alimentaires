1d4880dc4cd58a0649b2a90ab50f95c2d1246b23
Perrine Letellier <perrine.letellier@pm.me>
2025-11-24 15:58:19 +0100

    remove obsolete email address

--- frontend/src/views/CGUPage.vue
+++ frontend/src/views/CGUPage.vue
@@ -95,7 +95,7 @@
       La validation des CGU implique l’inscription automatique à l’infolettre Compl'Alim. Conforment à la
       réglementation, cette inscription est révocable à tout moment, soit en vous désabonnant à réception du courriel,
       ou en nous notifiant votre refus à l’adresse suivante :
-      <a href="mailto:compl-alim@beta.gouv.fr">compl-alim@beta.gouv.fr</a>
+      <a href="mailto:contact@compl-alim.beta.gouv.fr">contact@compl-alim.beta.gouv.fr</a>
     </p>
     <h2>Statistiques</h2>
     <p>Les données récoltées dans le cadre du téléservice servent au pilotage de la politique publique.</p>
@@ -152,15 +152,15 @@
     <ul>
       <li>
         Difficultés techniques :
-        <a href="mailto:compl-alim@beta.gouv.fr">compl-alim@beta.gouv.fr</a>
+        <a href="mailto:contact@compl-alim.beta.gouv.fr">contact@compl-alim.beta.gouv.fr</a>
       </li>
       <li>
         Problèmes liés à la procédure :
-        <a href="mailto:compl-alim@beta.gouv.fr">compl-alim@beta.gouv.fr</a>
+        <a href="mailto:contact@compl-alim.beta.gouv.fr">contact@compl-alim.beta.gouv.fr</a>
       </li>
       <li>
         Droit d’accès CNIL :
-        <a href="mailto:compl-alim@beta.gouv.fr">compl-alim@beta.gouv.fr</a>
+        <a href="mailto:contact@compl-alim.beta.gouv.fr">contact@compl-alim.beta.gouv.fr</a>
       </li>
     </ul>
   </div>
