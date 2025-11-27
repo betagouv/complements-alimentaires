8b03a31fb965992340fcb65ca54711b1c43df475
Helen Root <helenroot1@gmail.com>
2025-11-27 15:46:19 +0100

    Use enum instead of hardcoded email template ids

--- config/email.py
+++ config/email.py
@@ -1,3 +1,4 @@
+import enum
 from django.conf import settings
 
 import sib_api_v3_sdk
@@ -6,6 +7,28 @@ configuration = sib_api_v3_sdk.Configuration()
 configuration.api_key["api-key"] = settings.ANYMAIL.get("SENDINBLUE_API_KEY")
 api_client = sib_api_v3_sdk.ApiClient(configuration)
 email_api_instance = sib_api_v3_sdk.TransactionalEmailsApi(api_client)
+
+
+class EmailTemplateID(enum.Enum):
+    USER_ADDED_TO_COMPANY = 18
+    CONFIRM_EMAIL = 19
+    REQUEST_TO_JOIN_EMPTY_COMPANY = 15
+    REQUEST_TO_JOIN_COMPANY = 12
+    ACCEPT_REQUEST_TO_JOIN_COMPANY = 13
+    REFUSE_REQUEST_TO_JOIN_COMPANY = 14
+    INVITE_TO_JOIN_COMPANY = 16
+    COMPANY_INVITATION_ACCEPTED = 17
+    COMPANY_GIVEN_MANDATE = 27
+    # Declaration flow
+    DECLARATION_SUBMISSION_CONFIRMATION = 3
+    DECLARATION_OBSERVATION = 4
+    DECLARATION_OBJECTION = 5
+    DECLARATION_AUTHORIZED = 6
+    DECLARATION_WITHDRAWN = 8
+    DECLARATION_AUTHORIZATION_REVOKED = 37
+    DECLARATION_REJECTED = 7
+    DECLARATION_EXPIRATION_REMINDER = 10
+    DECLARATION_EXPIRED = 9
 
 
 def send_sib_template(template_id, parameters, to_email, to_name):
