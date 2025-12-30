5ae02b9c7ef19a84cc47fcf988c8550351443598
Alejandro MG <alejandro@amguillen.dev>
2025-12-30 12:28:30 +0100

    Refactor du fichier authentication.py

--- api/views/authentication.py
+++ api/views/authentication.py
@@ -13,30 +13,27 @@ class LoginView(APIView):
         username_or_email = request.data.get("username")
         password = request.data.get("password")
 
+        incomplete_data_msg = "Veuillez fournir un identifiant et un mot de passe."
+        unauthorized_msg = "Ce couple identifiant/mot de passe ne permet pas de vous identifier."
+        unverified_msg = "Votre compte n'est pas encore vérifié. Veuillez vérifier vos e-mails reçus, et vos courriers indésirables."
+
         if not username_or_email or not password:
-            raise ProjectAPIException(non_field_errors=["Veuillez fournir un identifiant et un mot de passe."])
+            raise ProjectAPIException(non_field_errors=[incomplete_data_msg])
 
         try:
             user = get_user_model().objects.get(Q(username=username_or_email) | Q(email=username_or_email))
         except get_user_model().DoesNotExist:
-            user = None
+            raise ProjectAPIException(non_field_errors=[unauthorized_msg])
 
-        if user is not None:
-            authenticated_user = authenticate(request, username=user.username, password=password)
-            if authenticated_user is not None:
-                if not authenticated_user.is_verified:
-                    raise ProjectAPIException(
-                        non_field_errors=[
-                            "Votre compte n'est pas encore vérifié. Veuillez vérifier vos e-mails reçus, et vos courriers indésirables."
-                        ],
-                        extra={"user_id": authenticated_user.id},
-                    )
-                login(request, authenticated_user)  # will create the user session
-                return Response({"csrf_token": get_token(request)})
+        authenticated_user = authenticate(request, username=user.username, password=password)
+        if not authenticated_user:
+            raise ProjectAPIException(non_field_errors=[unauthorized_msg])
 
-        raise ProjectAPIException(
-            non_field_errors=["Ce couple identifiant/mot de passe ne permet pas de vous identifier."]
-        )
+        if not authenticated_user.is_verified:
+            raise ProjectAPIException(non_field_errors=[unverified_msg], extra={"user_id": authenticated_user.id})
+
+        login(request, authenticated_user)  # will create the user session
+        return Response({"csrf_token": get_token(request)})
 
 
 class LogoutView(APIView):
