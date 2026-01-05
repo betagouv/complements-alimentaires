76cb412af0255c9da32b80fb02fe2045a23ca932
Helen Root <helenroot1@gmail.com>
2026-01-05 11:30:06 +0100

    Make tab stepper titles more unambiguous

--- frontend/src/components/TabStepper.vue
+++ frontend/src/components/TabStepper.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="border p-4 bg-gray-50 text-right relative">
     <slot name="content"></slot>
-    <DsfrButton secondary v-if="selectedTabIndex > 0" @click="emit('back')" :label="backLabel" />
-    <DsfrButton class="ml-4" v-if="selectedTabIndex < titles.length - 1" @click="emit('forward')" :label="fwdLabel" />
+    <DsfrButton secondary v-if="selectedTabIndex > 0" @click="emit('back')">
+      <span v-if="props.removeSaveLabel">Revenir</span>
+      <span v-else>
+        Sauvegarder
+        <span class="fr-sr-only">« {{ currentTabTitle }} »</span>
+        et revenir
+      </span>
+      a l'onglet « {{ previousTabTitle }} »
+    </DsfrButton>
+    <DsfrButton class="ml-4" v-if="selectedTabIndex < titles.length - 1" @click="emit('forward')">
+      <span v-if="props.removeSaveLabel">Passer</span>
+      <span v-else>
+        Sauvegarder
+        <span class="fr-sr-only">« {{ currentTabTitle }} »</span>
+        et passer
+      </span>
+      a l'onglet « {{ nextTabTitle }} »
+    </DsfrButton>
   </div>
 </template>
 
@@ -12,13 +28,7 @@ import { computed } from "vue"
 const props = defineProps(["titles", "selectedTabIndex", "removeSaveLabel"])
 const emit = defineEmits(["back", "forward"])
 
-const backLabel = computed(() => {
-  if (props.removeSaveLabel) return `Revenir à l'onglet « ${props.titles[props.selectedTabIndex - 1].title} »`
-  return `Sauvegarder et revenir à l'onglet « ${props.titles[props.selectedTabIndex - 1].title} »`
-})
-
-const fwdLabel = computed(() => {
-  if (props.removeSaveLabel) return `Passer à l'onglet « ${props.titles[props.selectedTabIndex + 1].title} »`
-  return `Sauvegarder et passer à l'onglet « ${props.titles[props.selectedTabIndex + 1].title} »`
-})
+const previousTabTitle = computed(() => props.titles[props.selectedTabIndex - 1].title)
+const currentTabTitle = computed(() => props.titles[props.selectedTabIndex].title)
+const nextTabTitle = computed(() => props.titles[props.selectedTabIndex + 1].title)
 </script>
