0afd2a531895f1daf95794c03dc01d64f5d93488
Helen Root <helenroot1@gmail.com>
2026-01-05 12:35:47 +0100

    Make modify buttons links with explicit accessible names

--- frontend/src/components/DeclarationSummary/SummaryModificationButton.vue
+++ frontend/src/components/DeclarationSummary/SummaryModificationButton.vue
@@ -1,3 +1,11 @@
 <template>
-  <DsfrButton secondary label="Modifier" size="small" />
+  <router-link :to="to" class="fr-btn fr-btn--secondary fr-btn--sm">
+    <span class="fr-sr-only">Retour à l'onglet {{ tabName }} pour</span>
+    Modifier
+    <span v-if="suffix" class="fr-sr-only">{{ suffix }}</span>
+  </router-link>
 </template>
+
+<script setup>
+defineProps(["to", "tabName", "suffix"])
+</script>
