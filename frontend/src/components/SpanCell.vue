81784d9819133bd0bcfe7a4a9f4708b8fbaec0dd
Helen Root <helenroot1@gmail.com>
2026-01-13 13:22:49 +0100

    Add docs to explain obscure component

--- frontend/src/components/SpanCell.vue
+++ frontend/src/components/SpanCell.vue
@@ -3,5 +3,16 @@
 </template>
 
 <script setup>
+// Ce composant existe pour appliquer des styles à un cellule d'un DsfrTable
+/*
+rowData = [
+  {
+    component: SpanCell,
+    text: "Ma phrase ici",
+    class: "capitalize",
+    id: "un-id-optionnel",
+  },
+]
+*/
 defineProps({ text: String })
 </script>
