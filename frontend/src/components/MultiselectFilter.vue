c209d781112b51d883c7c73dbf1793dd6ffba137
Helen Root <helenroot1@gmail.com>
2026-03-20 10:58:18 +0100

    Return multiselect filter list inline

--- frontend/src/components/MultiselectFilter.vue
+++ frontend/src/components/MultiselectFilter.vue
@@ -7,7 +7,7 @@
       {{ filterTitle }}
 
       <span v-if="selectedOptions.length">
-        <ul class="list-none pl-0 my-0" role="list">
+        <ul class="list-none pl-0 my-0 inline" role="list">
           <li v-for="option in selectedOptions" :key="`filter-opt-${option}`" class="inline">
             <DsfrTag class="mr-2 mt-1" :label="findLabel(option)" small />
           </li>
