df25177cfd2dcc10ed22241d1266ef6c481e0c4f
Alejandro MG <alejandro@amguillen.dev>
2026-08-09 21:57:23 +0200

    Revert "Enlève le template tag qui n'est plus utilisé"
    This reverts commit 3ce3d8c719061afec50f6d7873cd66cf6fc15bf6.

---
+++ web/templatetags/environment.py
@@ -0,0 +1,19 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.simple_tag
+def environment():
+    return getattr(settings, "ENVIRONMENT", "")
+
+
+@register.simple_tag
+def observation_days():
+    return getattr(settings, "OBSERVATION_DAYS")
+
+
+@register.simple_tag
+def contact_email():
+    return getattr(settings, "CONTACT_EMAIL")
