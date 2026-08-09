cdb500388845447a425e7e35691afd845deb0a13
Alejandro MG <alejandro@amguillen.dev>
2026-08-09 17:39:38 +0200

    Passage au type:module et suppression de eslintrc

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import tailwindcss from "@tailwindcss/vite"
-import path from "path"
+import { fileURLToPath, URL } from "url"
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": fileURLToPath(new URL("src", import.meta.url)),
     },
     extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
   },
