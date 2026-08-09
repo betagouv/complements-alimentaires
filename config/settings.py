e7a1339751e2ebb348c1e5c48d4ccee86fb8f8d1
Alejandro MG <alejandro@amguillen.dev>
2026-08-09 22:06:03 +0200

    Ajout du path platform dans le static url

--- config/settings.py
+++ config/settings.py
@@ -190,7 +190,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/platform/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
